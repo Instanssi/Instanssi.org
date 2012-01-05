2cec154295cafc201da6373e09e06a7da13d4bd2
Janne Leppänen <janne-leppanen@hotmail.com>
2012-01-05 15:46:39 +0200

    layout updated

--- Instanssi/main2012/static/js/instanssiBackground.js
+++ Instanssi/main2012/static/js/instanssiBackground.js
@@ -3,14 +3,19 @@ var _x = 0;
 var _y = 0;
 	
 $(document).ready( function(){
-	centerContent();
-	setFooterPosition();
+	/*centerContent();*/
+	/*setFooterPosition();*/
 	updateLayerItems();
-	$(window).resize(function() {centerContent();});
+	socialMediaButtons();
+	titleHover("#hearder h1");
+	siteMapHover();
+	$(window).resize(function() {/*centerContent();*/});
 	
 	$(document).mousemove(function(e){
 		_x = e.pageX; _y = e.pageY;
-		updateLayerItems();			
+		updateLayerItems();	
+	
+	
    });
 });
 
@@ -51,4 +56,50 @@ function setFooterPosition()
 		var newY = parseInt($(this).attr("y")) - tempHeight + 800;
 		$(this).attr("y", newY); 
 	});
+}
+
+function socialMediaButtons()
+{
+	$('.social-media img').mouseenter(function(){
+		$(this).stop().animate({top: "-3", opacity: 0.8}, 200);
+	});
+	$('.social-media img').mouseleave(function(){
+		$(this).stop().animate({top: "0", opacity: 1}, 200);
+	});
+}
+
+function titleHover( target ){
+	$( "#header a" ).mouseenter(function(){
+		$(this).stop().animate({opacity: 0.6}, 200);
+	});
+	$( "#header a" ).mouseleave(function(){
+		$(this).stop().animate({opacity: 1}, 200);
+	});
+}
+
+function siteMapHover(){
+	$( ".site-map a" ).mouseenter(function(){
+		$(this).stop().animate({"margin-left": "4px"}, 200);
+	});
+	$( ".site-map a" ).mouseleave(function(){
+		$(this).stop().animate({"margin-left": "0px"}, 200);
+	});
+}
+
+function setNavigationMarker( page )
+{
+	
+	var navX = 0;
+	switch( page ){
+		case "instanssi": 	navX = 25; break; 
+		case "aikataulu": 	navX = 25; break; 
+		case "kompot": 		navX = 25; break; 
+		case "liput": 		navX = 25; break; 
+		case "info": 		navX = 25; break; 
+		case "yhteystiedot":navX = 25; break; 
+		case "inenglish": 	navX = 25; break; 
+	}
+
+	
+	
 }

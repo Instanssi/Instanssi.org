6864a8eb76a5023d31baf7f67bc878e09c925976
Tuomas Virtanen <tuomas.virtanen@jyu.fi>
2012-01-05 20:47:54 +0200

    New twitter feed (Still needs work)

--- Instanssi/main2012/static/js/twitter_feed.js
+++ Instanssi/main2012/static/js/twitter_feed.js
@@ -1,53 +1,136 @@
-// Original from stackoverflow
-function parseTwitterDate(tdate) {
-    var system_date = new Date(Date.parse(tdate));
-    var user_date = new Date();
-    if (K.ie) {
-        system_date = Date.parse(tdate.replace(/( \+)/, ' UTC$1'))
+// Widget by bebraw, from https://github.com/bebraw/codegrove-site/blob/gh-pages/js/index.js
+// Some small modifications by Katajakasa
+
+function attr(k, v) {
+    // http://stackoverflow.com/questions/2010892/storing-objects-in-html5-localstorage
+    var target = sessionStorage;
+
+    if(!target) {
+        return null; // no support for localStorage :(
     }
-    var diff = Math.floor((user_date - system_date) / 1000);
-    if (diff <= 5) { return "just now"; }
-    if (diff < 60) { return "seconds ago"; }
-    if (diff <= 70) { return "one minute ago"; }
-    if (diff <= 3600-100) { return Math.round(diff / 60) + " minutes ago"; }
-    if (diff <= 3600+100) { return "1 hour ago"; }
-    if (diff <= 3600*11) { return Math.round(diff / 3600) + " hours ago"; }
-    if (diff <= 3600*13) { return "1 day ago"; }
-    if (diff < 3600*24*7) { return Math.round(diff / 86400) + " days ago"; }
-    if (diff <= 3600*24*8) { return "1 week ago"; }
-    if (diff <= 3600*24*7*4) { return "1 month ago"; }
-    if (diff < 3600*24*365) { return Math.round(diff / (3600*24*7*4)) + " months ago"; }
-    return "over a year ago";
+  
+    if(v !== undefined) {
+        target.setItem(k, JSON.stringify(v));
+        return null;
+    }
+        
+    try {
+        return JSON.parse(target.getItem(k));
+    }
+    catch(e) {}
+    
+    return target.getItem(k);
 }
 
-// from http://widgets.twimg.com/j/1/widget.js
-var K = function () {
-    var a = navigator.userAgent;
-    return {
-        ie: a.match(/MSIE\s([^;]*)/)
-    }
-}();
+function orderEntries(entries, limit) {
+    entries.sort(function(a, b) {
+        return a.publishedDate < b.publishedDate? 1: -1;
+    });
+    return entries.slice(0, limit);
+}
 
+function ISODateString(d){
+    function pad(n){
+        return n<10 ? '0'+n : n;
+    }
+    return d.getUTCFullYear()+'-' +
+        pad(d.getUTCMonth()+1)+'-'+
+        pad(d.getUTCDate());
+}
+
+// https://github.com/jamescarr/jquery-text-tools
+function linkify(text){
+    return text.replace(/(href="|<a.*?>)?[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+/g, function($0, $1) {
+        return $1 ? $0 : $0.link($0);
+    });
+}
+
+function getLatestTweets(user, limit, doneCb) {
+    var count = 0;
+    var tweetCache = attr('tweetCache');
+    var now = new Date();
+
+    if(tweetCache) {
+        if(new Date(now - new Date(tweetCache.time)).getHours() < 1) {
+            var tweets = tweetCache.tweets;
+
+            tweets = $.map(tweets, function(k, i) {
+                k.publishedDate = new Date(k.publishedDate);
+                return k;
+            });
+
+            doneCb(tweets);
+            return;
+        }
+    }
+    else {
+        tweetCache = {
+            time: now.getTime()
+        };
+    }
+
+    twitterlib.timeline(user, { limit: limit }, function(tweets) {
+        var ret = [];
+    
+        $.each(tweets, function(i, k) {
+            ret.push({
+                author: user,
+                text: linkify(k.text),
+                publishedDate: k.created_at
+            });
+        });
+
+        tweetCache.tweets = ret;
+        attr('tweetCache', tweetCache);
+
+        ret = $.map(ret, function(k, i) {
+            k.publishedDate = new Date(k.publishedDate);
+            return k;
+        });
+    
+        doneCb(ret);
+    });
+}
+
+function constructTweetUI($parent, entries) {
+    var $dl = $('<dl>').appendTo($parent);
+
+    $.each(entries, function(i, k) {
+        $('<dt>').append('<span class="date">' + ISODateString(k.publishedDate) + '</span>').
+            append('<span class="author">' + k.author + '</span>').appendTo($dl);
+        $('<dd>').append('<span class="title">' + k.text + '</span>').appendTo($dl);
+    });
+}
+
+function twitterWidget($parent, users, amount) {
+    var parsedData = [];
+    var found = 0;
+
+    $.each(users, function(i, user) {
+        getLatestTweets(user, 3, function(data) {
+            parsedData = parsedData.concat(data);
+            found++;
+      
+            if(found == users.length) {
+                var entries = orderEntries(parsedData, amount);
+                constructTweetUI($parent, entries);
+            }
+        });
+    });
+}
+
+// Hax.
 (function($){
 $.fn.twitterfeed = function(options) {
     var defaults = {
-        username: ""
+        users: [],
+        tweetcount: 5
     };
     var options = $.extend(defaults, options);
     
     return this.each(function() {
-        var obj = $(this);
-        var url = 'https://api.twitter.com/1/statuses/user_timeline.json?count=5&screen_name='+options.username+'&callback=?&trim_user=1';
-        $.getJSON(url, function(data) {
-            var out = "";
-            $.each(data, function(key, val) { 
-                out += '<p class="feedelement">';
-                out += val['text']+'<span class="twitter-date" >'+parseTwitterDate(val['created_at'])+'</span>';
-                out += '<span class="twitter-link"><a href="https://twitter.com/#!/'+options.username+'/status/'+val['id_str']+'">Open</a></span>';
-                out += '</p>';
-            }); 
-            obj.append(out);
-        });
+        twitterWidget($(this), options.users, options.tweetcount); 
     });
 };
 })(jQuery);
+

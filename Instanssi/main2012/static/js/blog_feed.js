aaf325ab2fbdd372c2fbe99a522c059fb0091029
Tuomas Virtanen <tuomas.virtanen@jyu.fi>
2012-01-06 00:35:51 +0200

    Start for blog feed

---
+++ Instanssi/main2012/static/js/blog_feed.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+(function($){
+$.fn.blogfeed = function(options) {
+    var defaults = {
+        address: 'http://blog.instanssi.org/feeds/posts/default',
+        postcount: 5
+    };
+    var options = $.extend(defaults, options);
+    
+    return this.each(function() {
+        
+    });
+};
+})(jQuery);

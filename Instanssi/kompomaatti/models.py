90543b95090fa96003e6640ffd16eb2a6a56f3e7
Tuomas Virtanen <tuomas.virtanen@jyu.fi>
2012-01-10 05:26:44 +0200

    Add some kompomaatti models (WIP)

--- Instanssi/kompomaatti/models.py
+++ Instanssi/kompomaatti/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib import admin
 
-# Create your models here.
+class Compo(models.Model):
+    name = models.CharField('Name', max_length=32)
+    description = models.TextField('Description')
+    add_deadline = models.DateTimeField('Deadline for uploads')
+    edit_deadline = models.DateTimeField('Deadline for edits')
+    time = models.DateTimeField('Compo time')
+    def __unicode__(self):
+        return self.name
+    
+class Entry(models.Model):
+    user = models.ForeignKey(User)
+    compo = models.ForeignKey(Compo)
+    name = models.CharField('Name', max_length=64)
+    description = models.TextField('Description')
+    creator = models.CharField('Creator name', max_length=64)
+    file = models.FileField(upload_to='entries/')
+    def __unicode__(self):
+        return self.name
+
+class Vote(models.Model):
+    user = models.ForeignKey(User)
+    compo = models.ForeignKey(Compo)
+    entry = models.ForeignKey(Entry)
+    def __unicode__(self):
+        return self.entry.name + ' by ' + self.user.name
+    
+admin.site.register(Compo)
+admin.site.register(Entry)
+admin.site.register(Vote)

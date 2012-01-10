f5c779107f961f2f97fa104b2cdb672387c99834
Tuomas Virtanen <tuomas.virtanen@jyu.fi>
2012-01-10 11:11:46 +0200

    Some work on Kompomaatti templates

--- Instanssi/kompomaatti/urls.py
+++ Instanssi/kompomaatti/urls.py
@@ -5,6 +5,8 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns(
     'kompomaatti.views',
     url(r'^$', 'index'),
-    url(r'^compo/', 'compo'),
-    url(r'^entry/', 'entry'),
+    url(r'^compo/(?P<compo_id>\d+)/', 'compo'),
+    url(r'^entry/(?P<entry_id>\d+)/', 'entry'),
+    url(r'^compolist/', 'compolist'),
+    url(r'^help/', 'help'),
 )
--- Instanssi/kompomaatti/views.py
+++ Instanssi/kompomaatti/views.py
@@ -1,12 +1,39 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render_to_response
+from models import Compo, Entry
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404  
+
+def custom_render(tpl, context={}):
+    compos = Compo.objects.all()
+    context['compos'] = compos
+    return render_to_response(tpl, context)
+
 
 def index(request):
-    return render_to_response('kompomaatti/index.html')
+    return custom_render('kompomaatti/index.html')
 
-def compo(request):
-    return render_to_response('kompomaatti/compo.html')
 
-def entry(request):
-    return render_to_response('kompomaatti/entry.html')
+def help(request):
+    return custom_render('kompomaatti/help.html')
+
+
+def compo(request, compo_id):
+    try:
+        compo = Compo.objects.get(id=compo_id)
+    except ObjectDoesNotExist:
+        raise Http404
+    return custom_render('kompomaatti/compo.html', {'compo': compo})
+
+
+def compolist(request):
+    return custom_render('kompomaatti/compolist.html')
+
+
+def entry(request, entry_id):
+    try:
+        entry = Entry.objects.get(id=entry_id)
+    except ObjectDoesNotExist:
+        raise Http404
+    return custom_render('kompomaatti/entry.html', {'entry': entry})

d0fa351c567ae8ecb0f8077bbe96ba04a3ed1ed3
Tuomas Virtanen <tuomas.virtanen@jyu.fi>
2012-01-10 04:06:41 +0200

    Kompomaatti app path changes

--- Instanssi/kompomaatti/urls.py
+++ Instanssi/kompomaatti/urls.py
@@ -4,5 +4,7 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns(
     'kompomaatti.views',
-    url(r'^$', 'index')
+    url(r'^$', 'index'),
+    url(r'^compo/', 'compo'),
+    url(r'^entry/', 'entry'),
 )
--- Instanssi/kompomaatti/views.py
+++ Instanssi/kompomaatti/views.py
@@ -3,4 +3,10 @@
 from django.shortcuts import render_to_response
 
 def index(request):
-    return render_to_response('kompo_index.html')
+    return render_to_response('kompomaatti/index.html')
+
+def compo(request):
+    return render_to_response('kompomaatti/compo.html')
+
+def entry(request):
+    return render_to_response('kompomaatti/entry.html')

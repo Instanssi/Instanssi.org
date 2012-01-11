1e812b6dbe2f24a9d50c73f0489f949b6392dabb
Tuomas Virtanen <tuomas.virtanen@jyu.fi>
2012-01-11 04:52:51 +0200

    More compolist, compo details stuff

--- Instanssi/kompomaatti/views.py
+++ Instanssi/kompomaatti/views.py
@@ -48,11 +48,16 @@ def compo(request, compo_id):
         compo = Compo.objects.get(id=compo_id)
     except ObjectDoesNotExist:
         raise Http404
-    return custom_render(request, 'kompomaatti/compo.html', {'compo': compo})
+    return custom_render(request, 'kompomaatti/compo.html', {
+        'compo': compo
+    })
 
 
 def compolist(request):
-    return custom_render(request, 'kompomaatti/compolist.html')
+    compos = Compo.objects.filter(active=True).order_by('compo_start')
+    return custom_render(request, 'kompomaatti/compolist.html', {
+        'compolist': compos,
+    })
 
 
 def entry(request, entry_id):
@@ -60,7 +65,9 @@ def entry(request, entry_id):
         entry = Entry.objects.get(id=entry_id)
     except ObjectDoesNotExist:
         raise Http404
-    return custom_render(request, 'kompomaatti/entry.html', {'entry': entry})
+    return custom_render(request, 'kompomaatti/entry.html', {
+        'entry': entry
+    })
 
 def dologout(request):
     logout(request)

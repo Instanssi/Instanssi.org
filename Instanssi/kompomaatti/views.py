cb299ec6e2e814fe37f8b26811c21efeaba0fae9
Tuomas Virtanen <tuomas.virtanen@jyu.fi>
2012-01-14 03:05:48 +0200

    Add some time handling magic, add some more voting system stuff.

--- Instanssi/kompomaatti/views.py
+++ Instanssi/kompomaatti/views.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render_to_response
-from models import Compo, Entry
+from models import Compo, Entry, Vote
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404, HttpResponseRedirect
 from forms import AddEntryForm
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
+from Instanssi.kompomaatti.misc import awesometime
 
 def custom_render(request, tpl, context={}):
     compos = Compo.objects.all()
@@ -25,6 +27,7 @@ def help(request):
 
 @login_required
 def myprods(request): 
+    # Handle adding an antry
     if request.method == 'POST':
         addform = AddEntryForm(request.POST, request.FILES) 
         if addform.is_valid(): 
@@ -35,32 +38,91 @@ def myprods(request):
     else:
         addform = AddEntryForm() 
 
+    # Get list of users entries
     my_entries = Entry.objects.filter(user=request.user)
 
+    # Dump the page to the user
     return custom_render(request, 'kompomaatti/myprods.html', {
         'addform': addform,
         'myentries': my_entries
     })
 
 
+def compo_times_formatter(compo):
+    compo.compo_time = awesometime.format_single(compo.compo_start)
+    compo.adding_time = awesometime.format_single(compo.adding_end)
+    compo.editing_time = awesometime.format_single(compo.editing_end)
+    compo.voting_time = awesometime.format_between(compo.voting_start, compo.voting_end)
+    return compo
+
 def compo(request, compo_id):
+    # Get compo information
     try:
-        c = Compo.objects.get(id=compo_id)
+        c = Compo.objects.get(id=compo_id, active=True)
     except ObjectDoesNotExist:
         raise Http404
     
-    e = Entry.objects.filter(compo=c)
+    # Format times and stuff
+    c = compo_times_formatter(c)
+    
+    # The following is only relevant, if the user is logged in and valid.
+    has_voted = False
+    voting_open = False
+    if request.user.is_authenticated():
+        # Check if user has already voted
+        votes = Vote.objects.filter(user=request.user, compo=c).order_by('rank')
+        if votes.count() > 0:
+            has_voted = True
+        
+        # Check if voting is open
+        now = datetime.now()
+        if c.voting_start <= now and now < c.voting_end:
+            voting_open = True
+    
+        # Handle voting
+        if request.method == 'POST':
+            if voting_open:
+                if has_voted:
+                    Vote.objects.delete(user=request.user, compo=c)
+                return HttpResponseRedirect('/kompomaatti/compo/'+compo_id+'/')
+            else:
+                raise Http404
+    
+    # Get entries.
+    # If voting is open, and user has already voted, get the order of entries by previous voting
+    # If voting is open, and user has NOT voted yet, get the entries in random order
+    # Otherwise just get entries and sort by name
+    if voting_open and has_voted:
+        e = []
+        for vote in votes:
+            e.append(vote.entry)
+    elif voting_open:
+        e = Entry.objects.filter(compo=c).order_by('?')
+    else:
+        e = Entry.objects.filter(compo=c).order_by('name')
+    
+    # Render the page. Ya, rly.
     return custom_render(request, 'kompomaatti/compo.html', {
         'compo': c,
-        'entries': e
+        'entries': e,
+        'voting_open': voting_open
     })
 
 
 def compolist(request):
-    compos = Compo.objects.filter(active=True).order_by('compo_start')
+    # Get compos, format times
+    composet = Compo.objects.filter(active=True).order_by('compo_start')
+    compos = []
+    for compo in composet:
+        compos.append(compo_times_formatter(compo))
+    
+    # Get entries in compos
+    # TODO: Get order by voting results
     entries = {}
     for compo in compos:
         entries[compo.id] = Entry.objects.filter(compo=compo)
+
+    # Return page
     return custom_render(request, 'kompomaatti/compolist.html', {
         'compolist': compos,
         'entries': entries,
@@ -68,10 +130,12 @@ def compolist(request):
 
 
 def entry(request, entry_id):
+    # Get the entry. Show 404 if it doesn't exist ...
     try:
         entry = Entry.objects.get(id=entry_id)
     except ObjectDoesNotExist:
         raise Http404
+    
     return custom_render(request, 'kompomaatti/entry.html', {
         'entry': entry
     })

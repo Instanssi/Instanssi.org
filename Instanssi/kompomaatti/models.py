5284c354c572077b7825f7b6d91d8a52315f7dbd
Tuomas Virtanen <tuomas.virtanen@jyu.fi>
2012-01-23 01:14:20 +0200

    Some title and help_text changes on Compo.entry_view_type

--- Instanssi/kompomaatti/models.py
+++ Instanssi/kompomaatti/models.py
@@ -29,7 +29,7 @@ class Compo(models.Model):
         (2, u'Vain kuva'), # Grafiikkakompoille
         (3, u'jPlayer ensin, sitten kuva'), # Musiikkikompoille
     )
-    entry_view_type = models.IntegerField(u'Entryesittelyn tyyppi', choices=ENTRY_VIEW_TYPES, default=0, help_text=u"Millainen näkymä näytetään entryn tiedoissa.")
+    entry_view_type = models.IntegerField(u'Entryesittely', choices=ENTRY_VIEW_TYPES, default=0, help_text=u"Millainen näkymä näytetään entryn tiedoissa? Prioriteetti ja tyyppi.")
     
     def __unicode__(self):
         return self.name

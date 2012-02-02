1ae1ffc51d9670ce5f477f6f5879f4c1d4447b01
Tuomas Virtanen <tuomas.virtanen@jyu.fi>
2012-02-02 06:56:44 +0200

    Upcoming events to the front page

--- Instanssi/kompomaatti/views/index.py
+++ Instanssi/kompomaatti/views/index.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from Instanssi.kompomaatti.misc.custom_render import custom_render
+from Instanssi.kompomaatti.models import Compo
+from datetime import datetime
 
 def index(request):
-    return custom_render(request, 'kompomaatti/index.html')
+    events = Compo.objects.filter(compo_start__gt=datetime.now()).order_by('compo_start')
+    
+    return custom_render(request, 'kompomaatti/index.html', {
+        'events': events
+    })

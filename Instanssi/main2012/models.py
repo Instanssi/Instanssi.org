6a8068f45a749de993716587642c09176ee764da
Tuomas Virtanen <tuomas.virtanen@jyu.fi>
2012-02-04 08:52:10 +0200

    Add author name field to BlogEntry

--- Instanssi/main2012/models.py
+++ Instanssi/main2012/models.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from django.contrib import admin
 
 class BlogEntry(models.Model):
     title = models.TextField(u'Otsikko')
     summary = models.TextField(u'Teksti')
     date = models.DateTimeField(u'Päivitetty')
     link = models.URLField(u'Linkki')
-    
+    name = models.CharField(u'Nimi', max_length=64)
+        
     def __unicode__(self):
         return self.title
     
     class Meta:
         verbose_name=u"blogientry"
         verbose_name_plural=u"blogientryt"
+        
+admin.site.register(BlogEntry)

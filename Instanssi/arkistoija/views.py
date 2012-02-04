fab4712bfca09e4102717356fe59a45344bb012b
Tuomas Virtanen <tuomas.virtanen@jyu.fi>
2012-02-04 18:32:47 +0200

    Arkistoija: A better structure for views.py file

--- Instanssi/arkistoija/views.py
+++ Instanssi/arkistoija/views.py
@@ -5,34 +5,69 @@ from django.http import Http404, HttpResponseRedirect, HttpResponse
 from Instanssi import settings
 import Instanssi.kompomaatti.models as kmodels
 import Instanssi.arkisto.models as amodels
+from forms import EventForm
 
-pages = (
-    'arkistoija/index.html',
-    'arkistoija/event_settings.html'
-)
+def dummy(request):
+    return {}
+
+def handle_event(request):
+    form = EventForm()
+    return {'eventform': form}
 
 def index(request, pgnum = "0"):
     # Make sure the user is admin
     if not request.user.is_authenticated() or not request.user.is_superuser:
         raise Http404
     
+    # Set session stuff
+    if not request.session.has_key('arkistoija') or len(request.session['arkistoija']) == 0:
+        request.session['arkistoija'] = {
+            'maxpage': 0,
+            'event': {},
+        }
+    
+    # Structure
+    pages_nm = {
+        0: 'arkistoija/index.html',
+        1: 'arkistoija/event_settings.html',
+    }
+    pages_fx = {
+        0: dummy,
+        1: handle_event,
+    }
+    
     # Find template that we want to load
     pgidx = int(pgnum)
-    tpl = pages[pgidx]
+    if pgidx >= len(pages_nm):
+        raise Http404
     
-    # Display template
-    return render_to_response(tpl, {
+    # Prevent user from getting too far without passing through
+    # all forms
+    if pgidx > request.session['arkistoija']['maxpage']+1:
+        raise Http404
+    
+    # Set max page
+    if pgidx > request.session['arkistoija']['maxpage']:
+        request.session['arkistoija']['maxpage'] = pgidx
+    
+    # Call our handler
+    ext = pages_fx[pgidx](request)
+    
+    # Other vars
+    vars = {
         'page_this': pgidx,
         'page_next': pgidx+1,
         'page_prev': pgidx-1,
-    })
+    }
+    return render_to_response(pages_nm[pgidx], dict(ext.items() + vars.items()))
 
 def cancel(request):
     # Make sure the user is admin
     if not request.user.is_authenticated() or not request.user.is_superuser:
         raise Http404
     
-    # TODO: Cancelling the transfer op goes here
+    # Clear data
+    request.session['arkistoija'] = {}
     
     # Redirect after cancellation
     return HttpResponseRedirect("/arkistoija/")

0760e14c43f78e4233dba4e00ddea72b3ee151dd
Tuomas Virtanen <tuomas.virtanen@jyu.fi>
2012-02-04 15:26:59 +0200

    Buttons, basic flow of archiving operation.

--- Instanssi/arkistoija/urls.py
+++ Instanssi/arkistoija/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns(
     'arkistoija.views',
     url(r'^$', 'index'),
-    url(r'^index/', 'index'),
+    url(r'^cancel/', 'cancel'),
+    url(r'^p/(?P<pgnum>\d+)/', 'index'),
 )
--- Instanssi/arkistoija/views.py
+++ Instanssi/arkistoija/views.py
@@ -1,12 +1,39 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render_to_response
-from django.http import HttpResponse
+from django.http import Http404, HttpResponseRedirect, HttpResponse
 from Instanssi import settings
 import Instanssi.kompomaatti.models as kmodels
 import Instanssi.arkisto.models as amodels
 
-def index(request):
-    return render_to_response("arkistoija/index.html", {
+pages = (
+    'arkistoija/index.html',
+    'arkistoija/event_settings.html'
+)
+
+def index(request, pgnum = "0"):
+    # Make sure the user is admin
+    if not request.user.is_authenticated() or not request.user.is_superuser:
+        raise Http404
     
+    # Find template that we want to load
+    pgidx = int(pgnum)
+    tpl = pages[pgidx]
+    
+    # Display template
+    return render_to_response(tpl, {
+        'page_this': pgidx,
+        'page_next': pgidx+1,
+        'page_prev': pgidx-1,
     })
+
+def cancel(request):
+    # Make sure the user is admin
+    if not request.user.is_authenticated() or not request.user.is_superuser:
+        raise Http404
+    
+    # TODO: Cancelling the transfer op goes here
+    
+    # Redirect after cancellation
+    return HttpResponseRedirect("/arkistoija/")
+

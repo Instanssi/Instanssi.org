a97467df14f7e8fb4c4fc80c5139d39a85ab64cc
Tuomas Virtanen <tuomas.virtanen@jyu.fi>
2012-02-24 01:09:44 +0200

    Arkisto magic. Add entry view, add it to urls.py. Also move html code around so that everything looks nice & smooth & pink & like ponies AND RAINBOWS!

--- Instanssi/arkisto/urls.py
+++ Instanssi/arkisto/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns(
     'arkisto.views',
-    url(r'^$', 'index')
+    url(r'^$', 'index'),
+    url(r'^entry/(?P<entry_id>\d+)/', 'entry'),
 )
--- Instanssi/arkisto/views.py
+++ Instanssi/arkisto/views.py
@@ -1,6 +1,38 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render_to_response
+from Instanssi.kompomaatti.models import Event, Compo, Entry
+from django.template import RequestContext
+from django.http import Http404
+from Instanssi import settings
+
+def custom_render(request, tpl, context={}):
+    internal = {
+        'nav_events': Event.objects.filter(archived=True).order_by('date'),
+        'debugmode': settings.DEBUG,
+    }
+    return render_to_response(tpl, dict(context.items() + internal.items()), context_instance=RequestContext(request))
+
 
 def index(request):
-    return render_to_response('arkisto/index.html')
+    return custom_render(request, 'arkisto/index.html')
+
+
+def entry(request, entry_id):
+    '''
+    # Get the entry
+    try:
+        entry = Entry.objects.get(id=entry_id)
+    except Entry.DoesNotExist:
+        raise Http404
+    
+    # Make sure the entry belongs to an archived event
+    if entry.compo.event.archived != True:
+        raise Http404
+    
+    # Dump the page
+    return custom_render(request, 'arkisto/entry.html', {
+        'entry': entry,
+    })
+    '''
+    return custom_render(request, 'arkisto/entry.html')

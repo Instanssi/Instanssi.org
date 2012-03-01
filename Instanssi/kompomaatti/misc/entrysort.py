e4125a39df0402a923ab5ca93be839fb1a1f4ec8
Tuomas Virtanen <tuomas.virtanen@jyu.fi>
2012-03-01 02:27:18 +0200

    Kompomaatti: Helper for sorting entries

---
+++ Instanssi/kompomaatti/misc/entrysort.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Helper function for sorting by entry score
+def helper(object):
+    return object.get_score()
+
+# Sorts entries by score
+def sort_by_score(entries):
+    return sorted(entries, key=helper, reverse=True)

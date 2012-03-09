d08072374729adbde7a677b686535f3d8005f1bf
Tuomas Virtanen <tuomas.virtanen@jyu.fi>
2012-03-09 19:27:39 +0200

    Kompomaatti: Random fixes

--- Instanssi/kompomaatti/views/index.py
+++ Instanssi/kompomaatti/views/index.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from Instanssi.settings import ACTIVE_EVENT_ID
 
 def index(request):
-    events = Compo.objects.filter(compo_start__gt=datetime.now(), event=ACTIVE_EVENT_ID).order_by('compo_start')
+    events = Compo.objects.filter(compo_start__gt=datetime.now(), event=ACTIVE_EVENT_ID, active=True).order_by('compo_start')
     
     return custom_render(request, 'kompomaatti/index.html', {
         'events': events

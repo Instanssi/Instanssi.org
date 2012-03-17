cedbd78d189f2f7eed929dd3cd60035075d894b2
Tuomas Virtanen <tuomas.virtanen@jyu.fi>
2012-03-17 05:03:47 +0200

    admin_base: Add some table styles

--- Instanssi/admin_upload/views.py
+++ Instanssi/admin_upload/views.py
@@ -5,6 +5,7 @@ from django.http import Http404
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from models import UploadedFile
+from forms import UploadForm
 
 @login_required
 def index(request):
@@ -22,6 +23,15 @@ def upload(request):
     if not request.user.is_superuser:
         raise Http404
     
+    # Handle form data, if any
+    if request.method == 'POST':
+        uploadform = UploadForm(request.POST, request.FILES)
+        if uploadform.is_valid():
+            pass
+    else:
+        uploadform = UploadForm()
+    
     # Render response
     return render_to_response("admin_upload/upload.html", {
+        'uploadform': uploadform,
     }, context_instance=RequestContext(request))

1d486d8035e918a83dce5a70c83149a06d982a9f
unknown <Tuomas Virtanen@.(none)>
2012-07-11 00:26:19 +0300

    admin_calendar: Fix to work on the latest django-imagekit

--- Instanssi/admin_calendar/models.py
+++ Instanssi/admin_calendar/models.py
@@ -3,8 +3,8 @@
 from django.db import models
 from django.contrib import admin
 from django.contrib.auth.models import User
-from imagekit.models import ImageSpec
-from imagekit.processors import resize
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
 
 class CalendarEvent(models.Model):
     user = models.ForeignKey(User, verbose_name=u'Käyttäjä')
@@ -13,7 +13,7 @@ class CalendarEvent(models.Model):
     description = models.TextField(u'Kuvaus', help_text=u'Tapahtuman kuvaus.', blank=True)
     title = models.CharField(u'Otsikko', help_text=u'Lyhyt otsikko.', max_length=32)
     image_original = models.ImageField(u'Kuva', upload_to='calendar/images/', help_text=u"Kuva tapahtumalle.", blank=True)
-    image_small = ImageSpec([resize.Fit(48, 48)], image_field='imagefile_original', format='PNG')
+    image_small = ImageSpecField([ResizeToFill(48, 48)], image_field='imagefile_original', format='PNG')
     EVENT_TYPES = (
         (0, u'Aikaraja'),
         (1, u'Aikavaraus'),

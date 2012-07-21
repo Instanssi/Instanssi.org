4863401b80460df6d590687ad620019b6135637e
Tuomas Virtanen <katajakasa@gmail.com>
2012-07-21 21:50:26 +0300

    dbsettings: Some fixes to get & set methods

--- Instanssi/dbsettings/models.py
+++ Instanssi/dbsettings/models.py
@@ -16,16 +16,46 @@ class Setting(models.Model):
     class Meta:
         verbose_name=u"asetus"
         verbose_name_plural=u"asetukset"
-        
-    def get(self, key):
-        return self.objects.get(key=key)
+
+    @staticmethod
+    def get(key):
+        p = Setting.objects.get(key=key)
+        if p.type == 0:
+            return int(p.value)
+        elif p.type == 1:
+            if p.value == u"0":
+                return False
+            else:
+                return True
+        else:
+            return unicode(p.value)
     
-    def set(self, key, value, type):
+    @staticmethod
+    def set(key, value, t=-1):
+        # If type is not given, try to guess
+        if t == -1:
+            if type(value) == str or type(value) == unicode:
+                t = 2
+            elif type(value) == int or type(value) == long:
+                t = 0
+                value = str(value)
+            elif type(value) == bool:
+                t = 1
+                if value:
+                    value = u"1"
+                else:
+                    value = u"0"
+            else:
+                raise TypeError(u"Can not set a variable of given type!")
+        
+        # Get old if it exists, otherwise create new setting
         try:
-            setting = self.objects.get(key=key)
+            setting = Setting.objects.get(key=key)
         except:
             setting = Setting()
             setting.key = key
-            setting.type = type
+        
+        # Set type and value, then save.
+        setting.type = t
         setting.value = value
         setting.save()

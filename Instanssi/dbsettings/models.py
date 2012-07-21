a7c0c5664f5a3ee774e9ea560c2f7faa9e8dfdcc
Tuomas Virtanen <katajakasa@gmail.com>
2012-07-21 22:43:51 +0300

    dbsettings: Add group handling

--- Instanssi/dbsettings/models.py
+++ Instanssi/dbsettings/models.py
@@ -1,14 +1,17 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 
 class Setting(models.Model):
     key = models.CharField(u'Avain', help_text=u'Asetuksen avain', max_length=32)
+    group = models.CharField(u'Ryhmä', help_text=u'Ryhmän nimi', max_length=32)
     value = models.CharField(u'Arvo', help_text=u'Asetuksen arvo', max_length=128)
     SETTING_TYPES = (
         (0, u'Integer'),
         (1, u'Boolean'), 
         (2, u'String'),
     )
-    type = models.IntegerField(u'Tyyppi', help_text=u'Asetuksen tyyppi (Boolean, Integer, String).', choices=SETTING_TYPES)
+    type = models.IntegerField(u'Tyyppi', help_text=u'Asetuksen tyyppi (Boolean=1, Integer=0, String=2).', choices=SETTING_TYPES)
     
     def __unicode__(self):
         return self.key
@@ -16,10 +19,11 @@ class Setting(models.Model):
     class Meta:
         verbose_name=u"asetus"
         verbose_name_plural=u"asetukset"
+        unique_together = ("key", "group")
 
     @staticmethod
-    def get(key):
-        p = Setting.objects.get(key=key)
+    def get(key, group=u''):
+        p = Setting.objects.get(key=key, group=group)
         if p.type == 0:
             return int(p.value)
         elif p.type == 1:
@@ -29,31 +33,35 @@ class Setting(models.Model):
                 return True
         else:
             return unicode(p.value)
+        
+    @staticmethod
+    def get_by_group(group):
+        return Setting.objects.filter(group=group)
     
     @staticmethod
-    def set(key, value, t=-1):
-        # If type is not given, try to guess
-        if t == -1:
-            if type(value) == str or type(value) == unicode:
-                t = 2
-            elif type(value) == int or type(value) == long:
-                t = 0
-                value = str(value)
-            elif type(value) == bool:
-                t = 1
-                if value:
-                    value = u"1"
-                else:
-                    value = u"0"
+    def set(key, value, group=u''):
+        # Guess type
+        if type(value) == str or type(value) == unicode:
+            t = 2
+        elif type(value) == int or type(value) == long:
+            t = 0
+            value = str(value)
+        elif type(value) == bool:
+            t = 1
+            if value:
+                value = u"1"
             else:
-                raise TypeError(u"Can not set a variable of given type!")
+                value = u"0"
+        else:
+            raise TypeError(u"Can not set a variable of given type!")
         
         # Get old if it exists, otherwise create new setting
         try:
-            setting = Setting.objects.get(key=key)
+            setting = Setting.objects.get(key=key, group=group)
         except:
             setting = Setting()
             setting.key = key
+            setting.group = group
         
         # Set type and value, then save.
         setting.type = t

d89e086a4089b850c5268cdbee47109a4efefac5
Tuomas Virtanen <katajakasa@gmail.com>
2012-07-24 03:27:12 +0300

    admin_kompomaatti: New administration panel for Kompomaatti

---
+++ Instanssi/admin_kompomaatti/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls.defaults import patterns, url
+
+urlpatterns = patterns(
+    'Instanssi.admin_kompomaatti.views',
+    url(r'^$', 'index'),
+    url(r'^compos/', 'compos'),
+    url(r'^entries/', 'entries'),
+    url(r'^results/', 'results'),
+    url(r'^votecodes/', 'votecodes'),
+    url(r'^votecoderequests/', 'votecoderequests'),
+)
---
+++ Instanssi/admin_kompomaatti/views.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import render_to_response
+from django.http import Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
+
+@login_required(login_url='/control/auth/login/')
+def index(request):
+    # Make sure the user is staff.
+    if not request.user.is_staff:
+        raise Http404
+    
+    # Render response
+    return render_to_response("admin_kompomaatti/index.html", {
+    }, context_instance=RequestContext(request))
+    

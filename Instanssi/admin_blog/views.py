6a28da3de2742dac360beb619c269fab1eeb38f0
Tuomas Virtanen <katajakasa@gmail.com>
2012-07-29 02:16:58 +0300

    admin_blog: Fix event selection, save form contents on save in edit form.

--- Instanssi/admin_blog/views.py
+++ Instanssi/admin_blog/views.py
@@ -4,10 +4,12 @@ from django.shortcuts import render_to_response
 from django.http import Http404,HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from Instanssi.ext_blog.models import BlogEntry, BlogComment
+from Instanssi.kompomaatti.models import Event
 from forms import BlogEntryForm
 from datetime import datetime
 from Instanssi.settings import SHORT_LANGUAGE_CODE
 from Instanssi.admin_base.misc.custom_render import admin_render
+from Instanssi.admin_base.misc.eventsel import get_selected_event
 
 @login_required(login_url='/control/auth/login/')
 def index(request):
@@ -15,7 +17,9 @@ def index(request):
     if not request.user.is_staff:
         raise Http404
     
-    entries = BlogEntry.objects.all()
+    # Get events
+    selected_event_id = get_selected_event(request)
+    entries = BlogEntry.objects.filter(event_id = selected_event_id)
     
     # Render response
     return admin_render(request, "admin_blog/index.html", {
@@ -32,16 +36,26 @@ def write(request):
     if not request.user.has_perm('admin_blog.add_blogentry'):
         raise Http404
     
-    if request.method == 'POST':
-        form = BlogEntryForm(request.POST)
-        if form.is_valid():
-            entry = form.save(commit=False)
-            entry.date = datetime.now()
-            entry.user = request.user
-            entry.save()
-            return HttpResponseRedirect("/control/blog/")
-    else:
-        form = BlogEntryForm()
+    # Find event
+    try:
+        event = Event.objects.get(id=get_selected_event(request))
+    except:
+        event = None
+    
+    # Post
+    form = None
+    if event:
+        if request.method == 'POST':
+            form = BlogEntryForm(request.POST)
+            if form.is_valid():
+                entry = form.save(commit=False)
+                entry.event = event
+                entry.date = datetime.now()
+                entry.user = request.user
+                entry.save()
+                return HttpResponseRedirect("/control/blog/")
+        else:
+            form = BlogEntryForm()
     
     # Render response
     return admin_render(request, "admin_blog/write.html", {

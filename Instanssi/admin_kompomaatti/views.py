05daaffdb0cc1127f24c67fd01ac142cb6b282cd
Tuomas Virtanen <katajakasa@gmail.com>
2012-07-29 01:12:25 +0300

    admin_kompomaatti. Results view.

--- Instanssi/admin_kompomaatti/views.py
+++ Instanssi/admin_kompomaatti/views.py
@@ -8,6 +8,7 @@ from Instanssi.kompomaatti.models import Compo,Entry,VoteCodeRequest
 from Instanssi.admin_kompomaatti.forms import AdminCompoForm, AdminEntryForm, AdminEntryAddForm
 from Instanssi.admin_base.misc.custom_render import admin_render
 from Instanssi.admin_base.misc.eventsel import get_selected_event
+from Instanssi.kompomaatti.misc import entrysort
     
 @login_required(login_url='/control/auth/login/')
 def compo_browse(request):
@@ -162,8 +163,24 @@ def results(request):
     if not request.user.is_staff:
         raise Http404
     
+    # Get compos
+    selected_event_id = get_selected_event(request)
+    try:
+        compos = Compo.objects.filter(event_id=selected_event_id)
+    except Entry.DoesNotExist:
+        raise Http404
+    
+    # Get the entries
+    results = {}
+    for compo in compos:
+        results[compo.name] = entrysort.sort_by_score(Entry.objects.filter(compo=compo))
+    
+    print results
+    
     # Render response
-    return admin_render(request, "admin_kompomaatti/results.html", {})
+    return admin_render(request, "admin_kompomaatti/results.html", {
+        'results': results,
+    })
     
 @login_required(login_url='/control/auth/login/')
 def votecodes(request):

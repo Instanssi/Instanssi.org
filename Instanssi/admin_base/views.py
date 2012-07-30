2dd484154d25351079da5eaa84cb2d1a0224ce53
Tuomas Virtanen <katajakasa@gmail.com>
2012-07-30 23:15:47 +0300

    admin_base: Select latest event when logged in.

--- Instanssi/admin_base/views.py
+++ Instanssi/admin_base/views.py
@@ -2,9 +2,14 @@
 
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from Instanssi.admin_base.misc.eventsel import get_selected_event
 
 @login_required(login_url='/control/auth/login/')
 def index(request):
+    # Select latest event as default
+    print get_selected_event(request)
+    
+    # Redirect to events page
     return HttpResponseRedirect("/control/events/")
     
 @login_required(login_url='/control/auth/login/')

fd2cddd87cd45b6ff1c7dc46fb46e3798b3d42b8
Tuomas Virtanen <katajakasa@gmail.com>
2012-07-30 23:15:35 +0300

    admin_base: Fix event selection

--- Instanssi/admin_base/misc/eventsel.py
+++ Instanssi/admin_base/misc/eventsel.py
@@ -5,7 +5,7 @@ from Instanssi.kompomaatti.models import Event
 def get_selected_event(request):
     if 'm_event_id' not in request.session:
         try:
-            event = Event.object.latest('date')[0]
+            event = Event.objects.latest('date')
             request.session['m_event_id'] = event.id
         except:
             request.session['m_event_id'] = -1

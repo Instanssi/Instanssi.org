228051ec7cae84560bc5d488366ee20257618bde
Tuomas Virtanen <katajakasa@gmail.com>
2012-08-14 19:38:38 +0300

    admin_arkisto: Adding, editing and deleting of videos and video categories.

--- Instanssi/admin_arkisto/urls.py
+++ Instanssi/admin_arkisto/urls.py
@@ -8,4 +8,10 @@ urlpatterns = patterns(
     url(r'^archiver/', 'archiver'),
     url(r'^vids/', 'vids'),
     url(r'^vidcats/', 'cats'),
+    url(r'^addvid/', 'addvid'),
+    url(r'^addvidcat/', 'addcat'),
+    url(r'^deletevid/(?P<video_id>\d+)/', 'deletevid'),
+    url(r'^deletecat/(?P<category_id>\d+)/', 'deletecat'),
+    url(r'^editvid/(?P<video_id>\d+)/', 'editvid'),
+    url(r'^editcat/(?P<category_id>\d+)/', 'editcat'),
 )
--- Instanssi/admin_arkisto/views.py
+++ Instanssi/admin_arkisto/views.py
@@ -52,6 +52,81 @@ def vids(request):
     })
     
 @login_required(login_url='/control/auth/login/')
+def addvid(request):
+    # Make sure the user is staff.
+    if not request.user.is_staff:
+        raise Http404
+    
+    # Check for permissions
+    if not request.user.has_perm('admin_arkisto.add_othervideo'):
+        raise Http404
+    
+    # Handle form
+    if request.method == "POST":
+        vidform = VideoForm(request.POST)
+        if vidform.is_valid():
+            vidform.save()
+            return HttpResponseRedirect("/control/arkisto/vids/")
+    else:
+        vidform = VideoForm()
+    
+    # Render response
+    return admin_render(request, "admin_arkisto/addvid.html", {
+        'vidform': vidform,                                             
+    })
+    
+@login_required(login_url='/control/auth/login/')
+def editvid(request, video_id):
+    # Make sure the user is staff.
+    if not request.user.is_staff:
+        raise Http404
+    
+    # Check for permissions
+    if not request.user.has_perm('admin_arkisto.change_othervideo'):
+        raise Http404
+    
+    # Get Video
+    try:
+        video = OtherVideo.objects.get(id=video_id)
+    except:
+        raise Http404
+    
+    # Handle form
+    if request.method == "POST":
+        vidform = VideoForm(request.POST, instance=video)
+        if vidform.is_valid():
+            vidform.save()
+            return HttpResponseRedirect("/control/arkisto/vids/")
+    else:
+        vidform = VideoForm(instance=video)
+    
+    # Render response
+    return admin_render(request, "admin_arkisto/editvid.html", {
+        'vidform': vidform,
+        'vid': video,                                          
+    })
+    
+    
+@login_required(login_url='/control/auth/login/')
+def deletevid(request, video_id):
+    # Make sure the user is staff.
+    if not request.user.is_staff:
+        raise Http404
+    
+    # Check for permissions
+    if not request.user.has_perm('admin_arkisto.delete_othervideo'):
+        raise Http404
+    
+    # Attempt to delete video
+    try:
+        OtherVideo.objects.get(id=video_id).delete()
+    except:
+        pass
+    
+    # Redirect
+    return HttpResponseRedirect("/control/arkisto/vids/")
+    
+@login_required(login_url='/control/auth/login/')
 def cats(request):
     # Make sure the user is staff.
     if not request.user.is_staff:
@@ -65,3 +140,79 @@ def cats(request):
     return admin_render(request, "admin_arkisto/cats.html", {
         'categories': categories,
     })
+    
+@login_required(login_url='/control/auth/login/')
+def addcat(request):
+    # Make sure the user is staff.
+    if not request.user.is_staff:
+        raise Http404
+    
+    # Check for permissions
+    if not request.user.has_perm('admin_arkisto.add_othervideocategory'):
+        raise Http404
+    
+    # Handle form
+    if request.method == "POST":
+        catform = VideoCategoryForm(request.POST)
+        if catform.is_valid():
+            cat = catform.save(commit=False)
+            cat.event_id = get_selected_event(request)
+            cat.save()
+            return HttpResponseRedirect("/control/arkisto/vidcats/")
+    else:
+        catform = VideoCategoryForm()
+    
+    # Render response
+    return admin_render(request, "admin_arkisto/addcat.html", {
+        'catform': catform,
+    })
+    
+@login_required(login_url='/control/auth/login/')
+def editcat(request, category_id):
+    # Make sure the user is staff.
+    if not request.user.is_staff:
+        raise Http404
+    
+    # Check for permissions
+    if not request.user.has_perm('admin_arkisto.change_othervideocategory'):
+        raise Http404
+    
+    # Get category
+    try:
+        category = OtherVideoCategory.objects.get(id=category_id)
+    except:
+        raise Http404
+    
+    # Handle form
+    if request.method == "POST":
+        catform = VideoCategoryForm(request.POST, instance=category)
+        if catform.is_valid():
+            catform.save()
+            return HttpResponseRedirect("/control/arkisto/vidcats/")
+    else:
+        catform = VideoCategoryForm(instance=category)
+    
+    # Render response
+    return admin_render(request, "admin_arkisto/editcat.html", {
+        'catform': catform,
+        'cat': category,
+    })
+    
+@login_required(login_url='/control/auth/login/')
+def deletecat(request, category_id):
+    # Make sure the user is staff.
+    if not request.user.is_staff:
+        raise Http404
+    
+    # Check for permissions
+    if not request.user.has_perm('admin_arkisto.delete_othervideocategory'):
+        raise Http404
+    
+    # Attempt to delete category
+    try:
+        OtherVideoCategory.objects.get(id=category_id).delete()
+    except:
+        pass
+    
+    # Redirect
+    return HttpResponseRedirect("/control/arkisto/vidcats/")

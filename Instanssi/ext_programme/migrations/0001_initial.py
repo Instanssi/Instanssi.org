3e4c82b0824eb9e76ce5ae7b7089a8cbe2c4ed12
Tuomas Virtanen <katajakasa@gmail.com>
2012-08-15 04:31:25 +0300

    ext_programme: Add model

---
+++ Instanssi/ext_programme/migrations/0001_initial.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'ProgrammeEvent'
+        db.create_table('ext_programme_programmeevent', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('event', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['kompomaatti.Event'])),
+            ('description', self.gf('django.db.models.fields.TextField')()),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=64)),
+            ('presenters', self.gf('django.db.models.fields.CharField')(max_length=256)),
+            ('presenters_titles', self.gf('django.db.models.fields.CharField')(max_length=256)),
+            ('icon_original', self.gf('django.db.models.fields.files.ImageField')(max_length=100, blank=True)),
+            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75, blank=True)),
+            ('home_url', self.gf('django.db.models.fields.URLField')(max_length=200, blank=True)),
+            ('twitter_url', self.gf('django.db.models.fields.URLField')(max_length=200, blank=True)),
+            ('facebook_url', self.gf('django.db.models.fields.URLField')(max_length=200, blank=True)),
+            ('linkedin_url', self.gf('django.db.models.fields.URLField')(max_length=200, blank=True)),
+            ('wiki_url', self.gf('django.db.models.fields.URLField')(max_length=200, blank=True)),
+        ))
+        db.send_create_signal('ext_programme', ['ProgrammeEvent'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'ProgrammeEvent'
+        db.delete_table('ext_programme_programmeevent')
+
+
+    models = {
+        'ext_programme.programmeevent': {
+            'Meta': {'object_name': 'ProgrammeEvent'},
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'event': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['kompomaatti.Event']"}),
+            'facebook_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'home_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'icon_original': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'linkedin_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'presenters': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'presenters_titles': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'twitter_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'wiki_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'})
+        },
+        'kompomaatti.event': {
+            'Meta': {'object_name': 'Event'},
+            'archived': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '64'})
+        }
+    }
+
+    complete_apps = ['ext_programme']
--- Instanssi/ext_programme/models.py
+++ Instanssi/ext_programme/models.py
@@ -1,5 +1,34 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from django.contrib import admin
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
+from Instanssi.kompomaatti.models import Event
 
-# Create your models here.
+class ProgrammeEvent(models.Model):
+    event = models.ForeignKey(Event, verbose_name=u'Tapahtuma')
+    description = models.TextField(u'Kuvaus', help_text=u'Tapahtuman kuvaus.')
+    title = models.CharField(u'Otsikko', help_text=u'Lyhyt otsikko.', max_length=64)
+    presenters = models.CharField(u'Henkilöt', help_text=u'Esityksen pitäjät tms.', max_length=256)
+    presenters_titles = models.CharField(u'Nimitykset', help_text=u'Pitäjien nimitykset (Konsultti, jne.)', max_length=256)
+    icon_original = models.ImageField(u'Kuva', upload_to='programme/images/', help_text=u"Kuva tapahtumalle.", blank=True)
+    icon_small = ImageSpecField([ResizeToFill(64, 64)], image_field='imagefile_original', format='PNG')
+    email = models.EmailField(u'Sähköposti', help_text=u'Tapahtumaan liittyvä sähköposti-osoite (esim. esiintyjän).', blank=True)
+    home_url = models.URLField(u'Kotiurli', help_text=u'Tapahtumaan liittyvä URL.', blank=True)
+    twitter_url = models.URLField(u'Twitter', help_text=u'Tapahtumaan liittyvä Twitter-url.', blank=True)
+    facebook_url = models.URLField(u'Facebook', help_text=u'Tapahtumaan liittyvä facebook-url.', blank=True)
+    linkedin_url = models.URLField(u'LinkedIn', help_text=u'Tapahtumaan liittyvä LinkedIn-url.', blank=True)
+    wiki_url = models.URLField(u'Wikipedia', help_text=u'Tapahtumaan liittyvä Wikipedia-url.', blank=True)
+
+    def __unicode__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name=u"ohjelmatapahtuma"
+        verbose_name_plural=u"ohjelmatapahtumat"
+
+try:
+    admin.site.register(ProgrammeEvent)
+except:
+    pass

6186614373ad99f2ca9bc034b72c5e3519e9b851
Tuomas Virtanen <katajakasa@gmail.com>
2012-08-16 03:07:03 +0300

    admin_calendar: Redesign the model

--- Instanssi/ext_calendar/migrations/0001_initial.py
+++ Instanssi/ext_calendar/migrations/0001_initial.py
@@ -12,15 +12,11 @@ class Migration(SchemaMigration):
         db.create_table('ext_calendar_calendarevent', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('event', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['kompomaatti.Event'])),
-            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
             ('start', self.gf('django.db.models.fields.DateTimeField')()),
             ('end', self.gf('django.db.models.fields.DateTimeField')(blank=True)),
             ('description', self.gf('django.db.models.fields.TextField')(blank=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=32)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=64)),
             ('icon_original', self.gf('django.db.models.fields.files.ImageField')(max_length=100, blank=True)),
-            ('type', self.gf('django.db.models.fields.IntegerField')(default=0)),
-            ('linked_compo', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['kompomaatti.Compo'], null=True, blank=True)),
-            ('linked_programme', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['ext_programme.ProgrammeEvent'], null=True, blank=True)),
         ))
         db.send_create_signal('ext_calendar', ['CalendarEvent'])
 
@@ -31,42 +27,6 @@ class Migration(SchemaMigration):
 
 
     models = {
-        'auth.group': {
-            'Meta': {'object_name': 'Group'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
-        },
-        'auth.permission': {
-            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
-            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        },
-        'auth.user': {
-            'Meta': {'object_name': 'User'},
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
-            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
-        },
-        'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
         'ext_calendar.calendarevent': {
             'Meta': {'object_name': 'CalendarEvent'},
             'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
@@ -74,49 +34,8 @@ class Migration(SchemaMigration):
             'event': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['kompomaatti.Event']"}),
             'icon_original': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'linked_compo': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['kompomaatti.Compo']", 'null': 'True', 'blank': 'True'}),
-            'linked_programme': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['ext_programme.ProgrammeEvent']", 'null': 'True', 'blank': 'True'}),
             'start': ('django.db.models.fields.DateTimeField', [], {}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
-            'type': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
-        },
-        'ext_programme.programmeevent': {
-            'Meta': {'object_name': 'ProgrammeEvent'},
-            'description': ('django.db.models.fields.TextField', [], {}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'event': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['kompomaatti.Event']"}),
-            'facebook_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
-            'github_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
-            'home_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
-            'icon_original': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'linkedin_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
-            'presenters': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
-            'presenters_titles': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
-            'twitter_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
-            'wiki_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'})
-        },
-        'kompomaatti.compo': {
-            'Meta': {'object_name': 'Compo'},
-            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'adding_end': ('django.db.models.fields.DateTimeField', [], {}),
-            'compo_start': ('django.db.models.fields.DateTimeField', [], {}),
-            'description': ('django.db.models.fields.TextField', [], {}),
-            'editing_end': ('django.db.models.fields.DateTimeField', [], {}),
-            'entry_sizelimit': ('django.db.models.fields.IntegerField', [], {'default': '134217728'}),
-            'entry_view_type': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
-            'event': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['kompomaatti.Event']"}),
-            'formats': ('django.db.models.fields.CharField', [], {'default': "'zip|7z|gz|bz2'", 'max_length': '128'}),
-            'hide_from_archive': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
-            'show_voting_results': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'source_formats': ('django.db.models.fields.CharField', [], {'default': "'zip|7z|gz|bz2'", 'max_length': '128'}),
-            'source_sizelimit': ('django.db.models.fields.IntegerField', [], {'default': '134217728'}),
-            'voting_end': ('django.db.models.fields.DateTimeField', [], {}),
-            'voting_start': ('django.db.models.fields.DateTimeField', [], {})
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '64'})
         },
         'kompomaatti.event': {
             'Meta': {'object_name': 'Event'},
--- Instanssi/ext_calendar/models.py
+++ Instanssi/ext_calendar/models.py
@@ -10,20 +10,24 @@ from Instanssi.ext_programme.models import ProgrammeEvent
 
 class CalendarEvent(models.Model):
     event = models.ForeignKey(Event, verbose_name=u'Tapahtuma')
-    user = models.ForeignKey(User, verbose_name=u'Käyttäjä')
     start = models.DateTimeField(u'Alku', help_text=u'Tapahtuman alkamisaika.')
-    end = models.DateTimeField(u'Loppe', help_text=u'Tapahtuman loppumisaika.', blank=True)
+    end = models.DateTimeField(u'Loppu', help_text=u'Tapahtuman loppumisaika.', blank=True)
     description = models.TextField(u'Kuvaus', help_text=u'Tapahtuman kuvaus.', blank=True)
-    title = models.CharField(u'Otsikko', help_text=u'Lyhyt otsikko.', max_length=32)
+    title = models.CharField(u'Otsikko', help_text=u'Lyhyt otsikko.', max_length=64)
     icon_original = models.ImageField(u'Kuva', upload_to='calendar/images/', help_text=u"Kuva tapahtumalle.", blank=True)
-    icon_small = ImageSpecField([ResizeToFill(48, 48)], image_field='icon_original', format='PNG')
-    EVENT_TYPES = (
-        (0, u'Kompo'),
-        (1, u'Ohjelmatapahtuma')
-    )
-    type = models.IntegerField(u'Tyyppi', help_text=u'Tapahtuman tyyppi', choices=EVENT_TYPES, default=0)
-    linked_compo = models.ForeignKey(Compo, blank=True, null=True, verbose_name=u'Liittyvä kompo')
-    linked_programme = models.ForeignKey(ProgrammeEvent, blank=True, null=True, verbose_name=u'Liittyvä ohjelmatapahtuma')
+    icon_small = ImageSpecField([ResizeToFill(64, 64)], image_field='icon_original', format='PNG')
+    
+    def save(self, *args, **kwargs):
+        # Delete old icon file when editing
+        try:
+            this = CalendarEvent.objects.get(id=self.id)
+            if this.icon_original != self.icon_original:
+                this.icon_original.delete(save=False)
+        except: 
+            pass 
+            
+        # Continue with normal save
+        super(CalendarEvent, self).save(*args, **kwargs)
     
     def __unicode__(self):
         return self.title

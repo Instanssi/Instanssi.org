be3a435e6c3bba830f156ba1dd9978d1424ac18b
Tuomas Virtanen <katajakasa@gmail.com>
2012-10-28 06:35:53 +0200

    admin_base: Clear unnecessary stuff

--- Instanssi/admin_base/views.py
+++ Instanssi/admin_base/views.py
@@ -2,31 +2,13 @@
 
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from Instanssi.admin_base.misc.eventsel import get_selected_event
+from Instanssi.admin_base.misc.custom_render import admin_render
 
-@login_required(login_url='/control/auth/login/')
+@login_required(login_url='/manage/auth/login/')
 def index(request):
-    # Select latest event as default
-    get_selected_event(request)
-    
-    # Redirect to events page
-    return HttpResponseRedirect("/control/events/")
-    
-@login_required(login_url='/control/auth/login/')
-def eventchange(request, event_id):
-    # Get redirect path
-    if 'r' in request.GET:
-        r = request.GET['r']
-        if r[0] != "/":
-            r = "/control/"
-    else:
-        r = "/control/"
-        
-    # Set session variable
-    try:
-        request.session['m_event_id'] = int(event_id)
-    except:
+    # Make sure the user is staff.
+    if not request.user.is_staff:
         raise Http404
-        
-    # Redirect
-    return HttpResponseRedirect(r)
+    
+    # Render response
+    return admin_render(request, "admin_base/index.html", {})

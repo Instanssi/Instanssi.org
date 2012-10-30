3574d58055c4e135ede928ad7a77ef196a51c1cb
Tuomas Virtanen <katajakasa@gmail.com>
2012-10-30 23:27:29 +0200

    admin_profile: Clean up remnants

--- Instanssi/admin_profile/views.py
+++ Instanssi/admin_profile/views.py
@@ -7,15 +7,6 @@ from Instanssi.admin_base.misc.custom_render import admin_render
 from Instanssi.admin_profile.forms import PasswordChangeForm,InformationChangeForm
 
 @login_required(login_url='/manage/auth/login/')
-def index(request):
-    # Make sure the user is staff.
-    if not request.user.is_staff:
-        raise Http404
-    
-    # Render response
-    return admin_render(request, "admin_profile/index.html", {})
-
-@login_required(login_url='/manage/auth/login/')
 def password(request):
     # Make sure the user is staff.
     if not request.user.is_staff:

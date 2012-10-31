83a5d28668949b332b9b1bf9880fee11db2067a6
Tuomas Virtanen <katajakasa@gmail.com>
2012-10-31 16:43:54 +0200

    admin_base: If selected_event_id is set, get name too.

--- Instanssi/admin_base/misc/custom_render.py
+++ Instanssi/admin_base/misc/custom_render.py
@@ -5,11 +5,13 @@ from Instanssi.kompomaatti.models import Event
 from django.template import RequestContext
 
 def admin_render(request, tpl, context={}):
-    # Events
     context['navmenu_events'] = Event.objects.all()
-
-    # For redirects
     context['full_path'] = request.get_full_path()
+    
+    # H4x
+    if 'selected_event_id' in context:
+        event = Event.objects.get(pk=context['selected_event_id'])
+        context['selected_event_name'] = event.name 
 
     # Render
     return render_to_response(tpl, context, context_instance=RequestContext(request))

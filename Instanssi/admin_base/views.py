4fa3003b5a9ddf7bba841bfbe49de386a0611688
Tuomas Virtanen <katajakasa@gmail.com>
2012-11-02 20:28:16 +0200

    admin_base: Some style changes, and add some content to the front page!

--- Instanssi/admin_base/views.py
+++ Instanssi/admin_base/views.py
@@ -3,6 +3,7 @@
 from common.http import Http403
 from django.contrib.auth.decorators import login_required
 from Instanssi.admin_base.misc.custom_render import admin_render
+from Instanssi.kompomaatti.models import Event, VoteCodeRequest, Entry, Compo
 
 @login_required(login_url='/manage/auth/login/')
 def index(request):
@@ -10,5 +11,46 @@ def index(request):
     if not request.user.is_staff:
         raise Http403
     
+    # Flag that tells if there are any important notices for the user
+    important_flag = False
+    
+    # Check if there are any waiting votecode requests
+    vcreqs = []
+    events = Event.objects.all()
+    for event in events:
+        rcount = VoteCodeRequest.objects.filter(event=event).count()
+        if rcount > 0:
+            important_flag = True
+            vcreqs.append({
+                'count': rcount,
+                'event_id': event.id,
+                'event_name': event.name,
+            })
+            
+    # Find disk usage for entries
+    entries = Entry.objects.all()
+    disk_usage = 0
+    for entry in entries:
+        disk_usage += entry.entryfile.size
+        if entry.sourcefile:
+            disk_usage += entry.sourcefile.size
+        if entry.imagefile_original:
+            disk_usage += entry.imagefile_original.size
+            disk_usage += entry.imagefile_thumbnail.size
+            disk_usage += entry.imagefile_medium.size
+            
+    # Get some statistics
+    stats = {
+        'events': Event.objects.all().count(),
+        'entries': Entry.objects.all().count(),
+        'compos': Compo.objects.all().count(),
+        'space_usage': disk_usage,
+    }
+            
     # Render response
-    return admin_render(request, "admin_base/index.html", {})
+    return admin_render(request, "admin_base/index.html", {
+        'real_name': request.user.first_name + ' ' + request.user.last_name,
+        'important_flag': important_flag,
+        'vcreqs': vcreqs,
+        'stats': stats,
+    })

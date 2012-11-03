4cc8fcd8d18ba34be2c5b1b5311fdddfbb051ac6
Tuomas Virtanen <katajakasa@gmail.com>
2012-11-04 00:16:54 +0200

    main2012: Remove old rss-to-blog copy mechanism as it is now unnecessary.

--- Instanssi/main2012/urls.py
+++ Instanssi/main2012/urls.py
@@ -13,5 +13,4 @@ urlpatterns = patterns(
     url(r'^liput/', 'liput'),
     url(r'^stream/', 'stream'),
     url(r'^yhteystiedot/', 'yhteystiedot'),
-    url(r'^updateblog/', 'updateblog'),
 )
--- Instanssi/main2012/views.py
+++ Instanssi/main2012/views.py
@@ -3,8 +3,6 @@
 from django.shortcuts import render_to_response
 from django.conf import settings
 from django.http import HttpResponse
-import feedparser
-from models import BlogEntry
 from datetime import datetime, timedelta
 import time
 
@@ -19,8 +17,7 @@ def render_custom(tpl, ext={}):
 
 # Pages
 def index(request):
-    entries = BlogEntry.objects.all().order_by('date').reverse()
-    return render_custom("main2012/index.html", {'blogitems': entries})
+    return render_custom("main2012/index.html")
 
 def info(request):
     return render_custom("main2012/info.html")
@@ -45,34 +42,4 @@ def liput(request):
 
 def yhteystiedot(request):
     return render_custom('main2012/yhteystiedot.html')
-
-def updateblog(request):
-    # Get feed
-    url = 'http://blog.instanssi.org/feeds/posts/default'
-    feed = feedparser.parse(url)
-    
-    # Delete old entries
-    BlogEntry.objects.filter(locked=False).delete()
-    
-    # Get items that have changed
-    for item in feed['items']:
-        locked = False
-        try:
-            oldentry = BlogEntry.objects.get(title=item['title'])
-            locked = oldentry.locked
-        except BlogEntry.DoesNotExist:
-            pass
-        
-        if not locked:
-            timestamp = datetime.fromtimestamp(time.mktime(item['published_parsed'])) + timedelta(hours=2)
-            entry = BlogEntry()
-            entry.title = item['title']
-            entry.summary = item['summary']
-            entry.date = timestamp
-            entry.link = item['link']
-            entry.name = item['authors'][0]['name']
-            entry.save()
-        
-    # Just return "ok"
-    return HttpResponse("ok");
 

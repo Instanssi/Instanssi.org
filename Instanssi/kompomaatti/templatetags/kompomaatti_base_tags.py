7a56a42511bd47c574f818afd32488d343ac78f4
Tuomas Virtanen <katajakasa@gmail.com>
2012-11-04 19:07:08 +0200

    kompomaatti: Forgot this ...

--- Instanssi/kompomaatti/templatetags/kompomaatti_base_tags.py
+++ Instanssi/kompomaatti/templatetags/kompomaatti_base_tags.py
@@ -7,8 +7,14 @@ register = template.Library()
 
 @register.inclusion_tag('kompomaatti/compo_nav_items.html')
 def render_base_compos_nav(event_id):
-    return {'compos': Compo.objects.filter(active=True, event_id=event_id)}
+    return {
+        'event_id': event_id,
+        'compos': Compo.objects.filter(active=True, event_id=event_id)
+    }
 
 @register.inclusion_tag('kompomaatti/competition_nav_items.html')
 def render_base_competitions_nav(event_id):
-    return {'competitions': Competition.objects.filter(event_id=event_id)}
+    return {
+        'event_id': event_id,
+        'competitions': Competition.objects.filter(active=True, event_id=event_id)
+    }

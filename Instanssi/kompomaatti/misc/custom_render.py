3e14fb1c13dc9a48bbcf5cbc968b42c9909dac81
Tuomas Virtanen <katajakasa@gmail.com>
2012-11-04 16:28:39 +0200

    kompomaatti: Change custom_render

--- Instanssi/kompomaatti/misc/custom_render.py
+++ Instanssi/kompomaatti/misc/custom_render.py
@@ -1,22 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render_to_response
-from Instanssi.kompomaatti.models import Compo, VoteCode
 from django.template import RequestContext
-from Instanssi.dbsettings.models import Setting
+from django.shortcuts import get_object_or_404
+from Instanssi.kompomaatti.models import Event
 
 def custom_render(request, tpl, context={}):
-    active_event_id = Setting.get('active_event_id', 'events', -1)
-    context['compos'] = Compo.objects.filter(active=True, event=active_event_id)
-    context['logged'] = request.user.is_authenticated()
-    context['is_su'] = request.user.is_superuser
-    associated = False
-    votecode = None
-    try:
-        votecode = VoteCode.objects.get(associated_to=request.user)
-        associated = True
-    except:
-        pass
-    context['associated'] = associated
-    context['votecode'] = votecode
+    context['user'] = request.user
+    
+    # Get event name
+    if 'sel_event_id' in context:
+        event = get_object_or_404(Event, pk=context['sel_event_id'])
+        context['sel_event_name'] = event.name
+    
     return render_to_response(tpl, context, context_instance=RequestContext(request))

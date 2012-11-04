f86d3957de8b6ac77185c0321e6eb9f51ab79522
Tuomas Virtanen <katajakasa@gmail.com>
2012-11-04 16:29:54 +0200

    kompomaatti: Fix urls

--- Instanssi/kompomaatti/urls.py
+++ Instanssi/kompomaatti/urls.py
@@ -5,6 +5,13 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns(
     'Instanssi.kompomaatti.views',
     url(r'^$', 'index', name="kompomaatti-index"),
+    url(r'^compos/', 'compos', name="kompomaatti-compos"),
+    url(r'^compos/(?P<compo_id>\d+)/', 'compo_details', name="kompomaatti-compo"),
+    url(r'^competitions/', 'competitions', name="kompomaatti-competitions"),
+    url(r'^competition/(?P<competition_id>\d+)/', 'competition_details', name="kompomaatti-competition"),
+    url(r'^profile/', 'profile', name="kompomaatti-profile"),
+    url(r'^login/', 'login', name="kompomaatti-login"),
+    url(r'^logout/', 'do_logout', name="kompomaatti-logout"),
 )
 
 """

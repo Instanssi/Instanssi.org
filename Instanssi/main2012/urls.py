98c6a277a555bdcfa74a4bfb840877ec98e03e5f
Tuomas Virtanen <katajakasa@gmail.com>
2012-11-06 20:43:15 +0200

    main2012: Remove tons of old cruft, use context processor vars.

--- Instanssi/main2012/urls.py
+++ Instanssi/main2012/urls.py
@@ -4,13 +4,13 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns(
     'Instanssi.main2012.views',
-    url(r'^$', 'index'),
-    url(r'^info/', 'info'),
-    url(r'^aikataulu/', 'aikataulu'),
-    url(r'^english/', 'english'),
-    url(r'^kompot/', 'kompot'),
-    url(r'^ohjelma/', 'ohjelma'),
-    url(r'^liput/', 'liput'),
-    url(r'^stream/', 'stream'),
-    url(r'^yhteystiedot/', 'yhteystiedot'),
+    url(r'^$', 'pageloader', {'templatename': 'index'}),
+    url(r'^info/', 'pageloader', {'templatename': 'info'}),
+    url(r'^aikataulu/', 'pageloader', {'templatename': 'aikataulu'}),
+    url(r'^english/', 'pageloader', {'templatename': 'english'}),
+    url(r'^kompot/', 'pageloader', {'templatename': 'kompot'}),
+    url(r'^ohjelma/', 'pageloader', {'templatename': 'ohjelma'}),
+    url(r'^liput/', 'pageloader', {'templatename': 'liput'}),
+    url(r'^stream/', 'pageloader', {'templatename': 'stream'}),
+    url(r'^yhteystiedot/', 'pageloader', {'templatename': 'yhteystiedot'}),
 )
--- Instanssi/main2012/views.py
+++ Instanssi/main2012/views.py
@@ -1,45 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render_to_response
-from django.conf import settings
-from django.http import HttpResponse
-from datetime import datetime, timedelta
-import time
+from django.template import RequestContext
 
-# A nice wrapper
-def render_custom(tpl, ext={}):
-    vars = {
-        'googleapikey': getattr(settings, 'GOOGLEAPIKEY'),
-        'googleanalytics': getattr(settings, 'GOOGLEANALYTICS'),
-        'debugmode': getattr(settings, 'DEBUG'),
-    }
-    return render_to_response(tpl, dict(vars.items() + ext.items()))
-
-# Pages
-def index(request):
-    return render_custom("main2012/index.html")
-
-def info(request):
-    return render_custom("main2012/info.html")
-
-def aikataulu(request):
-    return render_custom('main2012/aikataulu.html')
-
-def english(request):
-    return render_custom('main2012/english.html')
-
-def kompot(request):
-    return render_custom('main2012/kompot.html')
-
-def stream(request):
-    return render_custom('main2012/stream.html')
-
-def ohjelma(request):
-    return render_custom('main2012/ohjelma.html')
-
-def liput(request):
-    return render_custom('main2012/liput.html')
-
-def yhteystiedot(request):
-    return render_custom('main2012/yhteystiedot.html')
-
+def pageloader(request, templatename):
+    return render_to_response('main2012/'+templatename+'.html', context_instance=RequestContext(request))

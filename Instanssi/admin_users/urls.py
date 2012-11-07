455cbfb967d4f4aa639c490450a297d155c09578
Tuomas Virtanen <katajakasa@gmail.com>
2012-11-07 23:25:11 +0200

    admin_users: Tweak urls a bit.

--- Instanssi/admin_users/urls.py
+++ Instanssi/admin_users/urls.py
@@ -4,8 +4,7 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns(
     'Instanssi.admin_users.views',
-    url(r'^$', 'users'),
-    url(r'^users/', 'users', name="admin-users"),
+    url(r'^$', 'users', name="admin-users"),
     url(r'^delete/(?P<su_id>\d+)/', 'delete', name="admin-users-delete"),
     url(r'^edit/(?P<su_id>\d+)/', 'edit', name="admin-users-edit"),
 )

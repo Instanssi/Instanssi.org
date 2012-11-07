9aabc3d2f66e3f712ef66dd17403d1dc6328f56e
Tuomas Virtanen <katajakasa@gmail.com>
2012-11-07 07:52:02 +0200

    Implement sorting based on "humanized" filesizes for tablesorter2.

--- Instanssi/static/libs/js/other/tablesorter-widgets.js
+++ Instanssi/static/libs/js/other/tablesorter-widgets.js
@@ -1,4 +1,38 @@
 $.tablesorter.addParser({ 
+    id: 'humanizedBytes', 
+    is: function(s) { 
+        var spaceindex = s.indexOf(" ");
+        if(spaceindex == -1) return false;
+        
+        var sp = s.split(" ");
+        if(sp.length != 2) return false;
+        var value = parseFloat(sp[0])
+        if(isNaN(value)) return false;
+
+        if(sp[1] == "B") return true;
+        if(sp[1] == "KB") return true;
+        if(sp[1] == "MB") return true;
+        if(sp[1] == "GB") return true;
+        
+        return false;
+    }, 
+    format: function(s) { 
+        var sp = s.split(" ");
+        var value = parseFloat(sp[0]);
+        if(isNaN(value)) return $.tablesorter.formatFloat(0);
+        
+        var outval = 0;
+        if(sp[1] == "B") outval = value;
+        if(sp[1] == "KB") outval = value * 1024;
+        if(sp[1] == "MB") outval = value * 1024 * 1024;
+        if(sp[1] == "GB") outval = value * 1024 * 1024 * 1024;
+
+        return $.tablesorter.formatFloat(outval);
+    }, 
+    type: 'numeric' 
+}); 
+
+$.tablesorter.addParser({ 
     id: 'fiDate', 
     is: function(s) { 
         var spaceindex = s.indexOf(" ");

eeb67ffeb077574d121ca3d1f9d47325b19dbde3
Tuomas Virtanen <katajakasa@gmail.com>
2012-11-08 23:52:40 +0200

    main2012: Update url generation, and start using static tag.

--- Instanssi/main2012/urls.py
+++ Instanssi/main2012/urls.py
@@ -4,13 +4,13 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns(
     'Instanssi.main2012.views',
-    url(r'^$', 'pageloader', {'templatename': 'index'}),
-    url(r'^info/', 'pageloader', {'templatename': 'info'}),
-    url(r'^aikataulu/', 'pageloader', {'templatename': 'aikataulu'}),
-    url(r'^english/', 'pageloader', {'templatename': 'english'}),
-    url(r'^kompot/', 'pageloader', {'templatename': 'kompot'}),
-    url(r'^ohjelma/', 'pageloader', {'templatename': 'ohjelma'}),
-    url(r'^liput/', 'pageloader', {'templatename': 'liput'}),
-    url(r'^stream/', 'pageloader', {'templatename': 'stream'}),
-    url(r'^yhteystiedot/', 'pageloader', {'templatename': 'yhteystiedot'}),
+    url(r'^$', 'pageloader', {'templatename': 'index'}, name="index"),
+    url(r'^info/', 'pageloader', {'templatename': 'info'}, name="info"),
+    url(r'^aikataulu/', 'pageloader', {'templatename': 'aikataulu'}, name="aikataulu"),
+    url(r'^english/', 'pageloader', {'templatename': 'english'}, name="english"),
+    url(r'^kompot/', 'pageloader', {'templatename': 'kompot'}, name="kompot"),
+    url(r'^ohjelma/', 'pageloader', {'templatename': 'ohjelma'}, name="ohjelma"),
+    url(r'^liput/', 'pageloader', {'templatename': 'liput'}, name="liput"),
+    url(r'^stream/', 'pageloader', {'templatename': 'stream'}, name="stream"),
+    url(r'^yhteystiedot/', 'pageloader', {'templatename': 'yhteystiedot'}, name="yhteystiedot"),
 )

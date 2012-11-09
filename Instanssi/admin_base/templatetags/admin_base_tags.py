76206bce36f016a5885f0b579c427e4037cfa30e
Tuomas Virtanen <katajakasa@gmail.com>
2012-11-09 02:47:45 +0200

    admin_base: Move tag templates to a subdirectory.

--- Instanssi/admin_base/templatetags/admin_base_tags.py
+++ Instanssi/admin_base/templatetags/admin_base_tags.py
@@ -5,7 +5,7 @@ from Instanssi.kompomaatti.models import Event
 
 register = template.Library()
 
-@register.inclusion_tag('admin_base/event_nav_items.html')
+@register.inclusion_tag('admin_base/tags/event_nav_items.html')
 def render_base_events_nav():
     return {'events': Event.objects.all().order_by('-date')}
 

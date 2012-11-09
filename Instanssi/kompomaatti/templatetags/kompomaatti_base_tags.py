77ff64c858aa21c9651ccf58c95b739db45cd97b
Tuomas Virtanen <katajakasa@gmail.com>
2012-11-09 02:40:21 +0200

    kompomaatti: Update tags to use new template subdirectory.

--- Instanssi/kompomaatti/templatetags/kompomaatti_base_tags.py
+++ Instanssi/kompomaatti/templatetags/kompomaatti_base_tags.py
@@ -5,14 +5,14 @@ from Instanssi.kompomaatti.models import Compo, Competition
 
 register = template.Library()
 
-@register.inclusion_tag('kompomaatti/compo_nav_items.html')
+@register.inclusion_tag('kompomaatti/tags/compo_nav_items.html')
 def render_base_compos_nav(event_id):
     return {
         'event_id': event_id,
         'compos': Compo.objects.filter(active=True, event_id=event_id)
     }
 
-@register.inclusion_tag('kompomaatti/competition_nav_items.html')
+@register.inclusion_tag('kompomaatti/tags/competition_nav_items.html')
 def render_base_competitions_nav(event_id):
     return {
         'event_id': event_id,

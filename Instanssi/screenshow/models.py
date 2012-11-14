734bdbc17029d031484f77c7afcdc834c421b04d
Tuomas Virtanen <katajakasa@gmail.com>
2012-11-14 23:54:09 +0200

    screenshow: Add new model for irc messages.

--- Instanssi/screenshow/models.py
+++ Instanssi/screenshow/models.py
@@ -13,3 +13,9 @@ class Message(models.Model):
     show_start = models.DateTimeField(u'Alkuaika', help_text=u'Viestin näyttäminen alkaa')
     show_end = models.DateTimeField(u'Loppuaika', help_text=u'Viestin näyttäminen päättyy')
     text = models.TextField(u'Viesti', help_text=u'Viestin leipäteksti. Katso ettei tästä tule liian pitkä.')
+    
+class IRCMessage(models.Model):
+    event = models.ForeignKey(Event, verbose_name=u'Tapahtuma')
+    date = models.DateTimeField(u'Aika')
+    nick = models.CharField(u'Nimimerkki', max_length=64)
+    message = models.TextField('Viesti')

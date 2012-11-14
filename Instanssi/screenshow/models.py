185359350fa3c480f00b4c4fd8751e2b498b2e97
Tuomas Virtanen <katajakasa@gmail.com>
2012-11-15 01:16:22 +0200

    screenshow: Some model tweaks.

--- Instanssi/screenshow/models.py
+++ Instanssi/screenshow/models.py
@@ -8,14 +8,44 @@ class Sponsor(models.Model):
     name = models.CharField(u'Nimi', max_length=64, help_text=u'Sponsorin nimi')
     logo = models.ImageField(u'Kuva', upload_to='screen/sponsorlogos/', help_text=u"Sponsorin logo", blank=True)
 
+    def __unicode__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name=u"sponsori"
+        verbose_name_plural=u"sponsorit"
+
+    def save(self, *args, **kwargs):
+        try:
+            this = Sponsor.objects.get(id=self.id)
+            if this.logo != self.logo:
+                this.logo.delete(save=False)
+        except: 
+            pass 
+            
+
 class Message(models.Model):
     event = models.ForeignKey(Event, verbose_name=u'Tapahtuma')
     show_start = models.DateTimeField(u'Alkuaika', help_text=u'Viestin näyttäminen alkaa')
     show_end = models.DateTimeField(u'Loppuaika', help_text=u'Viestin näyttäminen päättyy')
     text = models.TextField(u'Viesti', help_text=u'Viestin leipäteksti. Katso ettei tästä tule liian pitkä.')
     
+    def __unicode__(self):
+        return self.text[:16]+u' ...'
+    
+    class Meta:
+        verbose_name=u"viesti"
+        verbose_name_plural=u"viestit"
+    
 class IRCMessage(models.Model):
     event = models.ForeignKey(Event, verbose_name=u'Tapahtuma')
     date = models.DateTimeField(u'Aika')
     nick = models.CharField(u'Nimimerkki', max_length=64)
-    message = models.TextField('Viesti')
+    message = models.TextField(u'Viesti')
+    
+    def __unicode__(self):
+        return self.message[:16]+u' ...'
+    
+    class Meta:
+        verbose_name=u"irc-viesti"
+        verbose_name_plural=u"irc-viestit"

7a7abbaed772ab723a406571c6b932849fa4861f
Tuomas Virtanen <katajakasa@gmail.com>
2012-11-15 04:12:09 +0200

    screenshow: Print sponsors.

--- Instanssi/screenshow/views.py
+++ Instanssi/screenshow/views.py
@@ -5,9 +5,27 @@ from django.template import RequestContext
 from common.responses import JSONResponse
 from Instanssi.kompomaatti.models import Event
 from Instanssi.kompomaatti.misc.events import get_upcoming
+from Instanssi.screenshow.models import *
 
 def index(request, event_id):
-    return render_to_response('screenshow/index.html', {'event_id': event_id}, context_instance=RequestContext(request))
+    # Get sponsors
+    sponsors = []
+    x = 800
+    for sponsor in Sponsor.objects.filter(event_id=event_id):
+        sponsor.x = x
+        sponsor.z = 500
+        sponsor.y = -270
+        sponsor.rot_x = 0
+        sponsor.rot_y = 30
+        sponsor.rot_z = 90
+        sponsors.append(sponsor)
+        x -= 100
+    
+    # Render the show
+    return render_to_response('screenshow/index.html', {
+        'event_id': event_id,
+        'sponsors': sponsors,
+    }, context_instance=RequestContext(request))
 
 def events_api(request, event_id):
     e = get_object_or_404(Event, pk=event_id)

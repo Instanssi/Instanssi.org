bd0a4dd0dc23688b2001bbfd575a05b38af0379d
Tuomas Virtanen <katajakasa@gmail.com>
2012-11-17 00:32:41 +0200

    main2013: Switch to correct ID

--- Instanssi/main2013/views.py
+++ Instanssi/main2013/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def pageloader(request, templatename):
-    return render_to_response('main2013/'+templatename+'.html', {'event_id': 1}, context_instance=RequestContext(request))
+    return render_to_response('main2013/'+templatename+'.html', {'event_id': 5}, context_instance=RequestContext(request))

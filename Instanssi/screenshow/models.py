b6e127f9889ce6484e410af7249fcbbd337cb307
Tuomas Virtanen <katajakasa@gmail.com>
2012-11-19 18:48:28 +0200

    screenshow: Save sponsor logos as PNG to save transparency + for better quality.

--- Instanssi/screenshow/models.py
+++ Instanssi/screenshow/models.py
@@ -37,7 +37,7 @@ class Sponsor(models.Model):
     event = models.ForeignKey(Event, verbose_name=u'Tapahtuma')
     name = models.CharField(u'Nimi', max_length=64, help_text=u'Sponsorin nimi')
     logo = models.ImageField(u'Kuva', upload_to='screen/sponsorlogos/', help_text=u"Sponsorin logo", blank=True)
-    logo_scaled = ImageSpecField([ResizeToFit(800, 800, True)], image_field='logo', format='JPEG', options={'quality': 95})
+    logo_scaled = ImageSpecField([ResizeToFit(800, 800, True)], image_field='logo', format='PNG')
 
     def __unicode__(self):
         return self.name

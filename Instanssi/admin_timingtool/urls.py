72a58a1f8fcde8bb5a4978b67107307bc8919a6b
Tuomas Virtanen <katajakasa@gmail.com>
2012-11-24 05:46:28 +0200

    admin_timingtool: Add initial code.

---
+++ Instanssi/admin_timingtool/urls.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls.defaults import patterns, url
+
+urlpatterns = patterns(
+    'Instanssi.admin_timingtool.views',
+    url(r'^$', 'index', name="index"),
+)

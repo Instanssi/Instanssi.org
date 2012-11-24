e6fe6adca6e5a5379006d5554c997ff44d804ccc
Tuomas Virtanen <katajakasa@gmail.com>
2012-11-24 06:09:28 +0200

    admin_timingtool: Form tweaks

--- Instanssi/admin_timingtool/forms.py
+++ Instanssi/admin_timingtool/forms.py
@@ -7,17 +7,3 @@ from crispy_forms.layout import Submit, Layout, Fieldset, ButtonHolder
 class TimingForm(forms.Form):
     moveby = forms.TimeField(label=u'Kuinka paljon tapahtumia siirretään')
     direction = forms.ChoiceField(label=u'Siirron suunta', choices=[(0, u'Taaksepäin'), (1, u'Eteenpäin')])
-    
-    def __init__(self, *args, **kwargs):
-        super(TimingForm, self).__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.layout = Layout(
-            Fieldset(
-                u'Ajastus',
-                'moveby',
-                'direction',
-                ButtonHolder (
-                    Submit('submit', u'Muuta')
-                )
-            )
-        )

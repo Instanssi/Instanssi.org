0b97e1713c4b00dd4da7b13ee84f75f9fb43f508
Tuomas Virtanen <katajakasa@gmail.com>
2012-12-06 13:27:39 +0200

    admin_kompomaatti: Add image formats field to admin form.

--- Instanssi/admin_kompomaatti/forms.py
+++ Instanssi/admin_kompomaatti/forms.py
@@ -113,6 +113,7 @@ class AdminCompoForm(forms.ModelForm):
                 'source_sizelimit',
                 'formats',
                 'source_formats',
+                'image_formats',
                 'thumbnail_pref',
                 'active',
                 'show_voting_results',

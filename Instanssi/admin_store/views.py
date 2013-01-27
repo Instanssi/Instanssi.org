d7eb44d68f5ecc6c9a90e988cab8fd7ae65cdd4d
Tuomas Virtanen <katajakasa@gmail.com>
2013-01-27 02:00:24 +0200

    admin_store: Move urls elsewhere

--- Instanssi/admin_store/views.py
+++ Instanssi/admin_store/views.py
@@ -5,24 +5,27 @@ from django.shortcuts import get_object_or_404
 from django.core.urlresolvers import reverse
 from Instanssi.admin_base.misc.custom_render import admin_render
 from Instanssi.admin_base.misc.auth_decorator import staff_access_required
+from Instanssi.store.models import *
 
 # Logging related
 import logging
 logger = logging.getLogger(__name__)
 
 @staff_access_required
-def items(request, sel_event_id):
+def items(request):
+    items = StoreItem.objects.all()
 
     # Render response
     return admin_render(request, "admin_store/items.html", {
-        'selected_event_id': int(sel_event_id),
+        'items': items,
     })
     
     
 @staff_access_required
-def status(request, sel_event_id):
+def status(request):
+    transactions = StoreTransaction.objects.all()
 
     # Render response
     return admin_render(request, "admin_store/status.html", {
-        'selected_event_id': int(sel_event_id),
+        'transactions': transactions,
     })

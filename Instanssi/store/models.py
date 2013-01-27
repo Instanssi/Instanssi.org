3db3704edef050fab7f1444ddffca0b850aa4ff8
Tuomas Virtanen <katajakasa@gmail.com>
2013-01-27 05:06:26 +0200

    store: Add some helper methods

--- Instanssi/store/models.py
+++ Instanssi/store/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from django.db.models import Sum
 from django.contrib import admin
 from datetime import datetime
 from Instanssi.kompomaatti.models import Event
@@ -20,6 +21,16 @@ class StoreItem(models.Model):
     def __unicode__(self):
         return self.name
 
+    class Meta:
+        verbose_name=u"tuote"
+        verbose_name_plural=u"tuotteet"
+    
+    def sold(self):
+        res = TransactionItem.objects.filter(transaction__paid=1, item=self).aggregate(Sum('amount'))
+        if res['amount__sum'] == None:
+            return 0
+        return res['amount__sum']
+
 class StoreTransaction(models.Model):
     token = models.CharField(u'Palvelutunniste', max_length=255)
     time = models.DateTimeField(u'Aika')
@@ -37,12 +48,36 @@ class StoreTransaction(models.Model):
     country = models.CharField(u'Maa', max_length=2)
     
     def __unicode__(self):
-        return self.firstname + u' ' + self.lastname
+        return u'%s %s' % (self.firstname, self.lastname)
+    
+    def totalcost(self):
+        ret = 0.0
+        for item in TransactionItem.objects.filter(transaction=self):
+            ret += item.total()
+        return ret
+    
+    class Meta:
+        verbose_name=u"transaktio"
+        verbose_name_plural=u"transaktiot"
     
 class TransactionItem(models.Model):
     item = models.ForeignKey(StoreItem, verbose_name=u'Tuote')
     transaction = models.ForeignKey(StoreTransaction, verbose_name=u'Ostotapahtuma')
     amount = models.IntegerField(u'Ostettu')
     
+    def total(self):
+        return self.amount * self.item.price
+    
     def __unicode__(self):
-        return '"'+self.item.title + u'" for ' + self.item.transaction
+        return u'%s for %s %s' % (self.item.name, self.transaction.firstname, self.transaction.lastname)
+
+    class Meta:
+        verbose_name=u"transaktiotuote"
+        verbose_name_plural=u"transaktiotuotteet"
+
+try:
+    admin.site.register(StoreItem)
+    admin.site.register(StoreTransaction)
+    admin.site.register(TransactionItem)
+except:
+    pass

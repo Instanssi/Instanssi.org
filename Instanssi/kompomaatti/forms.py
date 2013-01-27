dad811c90c277085b11d182ba728a76ba50ae6f8
Tuomas Virtanen <katajakasa@gmail.com>
2013-01-27 08:51:34 +0200

    kompomaatti: Handle compo.thunbnail_pref, finally!

--- Instanssi/kompomaatti/forms.py
+++ Instanssi/kompomaatti/forms.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 
+import os
+from datetime import datetime
+
 from django import forms
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.core.files.base import File
+
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit, Layout, Fieldset, ButtonHolder
-from datetime import datetime
-from django.core.exceptions import ValidationError
-import os
-from django.contrib.auth.models import User
+
 from Instanssi.kompomaatti.misc.sizeformat import sizeformat
 from Instanssi.kompomaatti.models import Compo, Entry, VoteCode, VoteCodeRequest, Profile, CompetitionParticipation
-from django.core.urlresolvers import reverse
+
 
 class OpenIDLoginForm(forms.Form):
     sps = forms.ChoiceField(
@@ -180,25 +185,31 @@ class EntryForm(forms.ModelForm):
         # Max sizes for filefields
         self.max_source_size = self.compo.source_sizelimit
         self.max_entry_size = self.compo.entry_sizelimit
+        self.show_thumbnail_field = (self.compo.thumbnail_pref == 0 or self.compo.thumbnail_pref == 2)
         self.max_image_size = 6 * 1024 * 1024
-        self.imageformats = ['png','jpg']
         
         # Layout for uni-form ext.
         self.helper = FormHelper()
-        self.helper.layout = Layout(
-            Fieldset(
-                u'Entry',
-                'name',
-                'creator',
-                'description',
-                'entryfile',
-                'sourcefile',
-                'imagefile_original',
-                ButtonHolder (
-                    Submit('submit', u'Tallenna')
-                )
-            )
-        )
+        
+        # Fill the fieldset with the required fields
+        fs = Fieldset(u'Entry')
+        fs.fields.extend([
+            'name',
+            'creator',
+            'description',
+            'entryfile',
+            'sourcefile'
+        ])
+        
+        # Add thumbnail field if necessary
+        if self.show_thumbnail_field:
+            fs.fields.append('imagefile_original')  
+            
+        # Add submitbutton
+        fs.fields.append(ButtonHolder(Submit('submit', u'Tallenna')))
+        
+        # Add fieldset to layout
+        self.helper.layout = Layout(fs)
         
         # Create the fields
         super(EntryForm, self).__init__(*args, **kwargs)
@@ -219,44 +230,74 @@ class EntryForm(forms.ModelForm):
                 + u'. Tiedoston maksimikoko on ' \
                 + sizeformat(self.max_source_size) + '.'
         
-        # Description for imagefile
-        self.fields['imagefile_original'].help_text = \
-            u"Kuva teokselle. Tätä käytetään mm. arkistossa ja kompomaatin äänestysvaiheessa. Sallitut kuvaformaatit: " \
-            + ', '.join(self.imageformats)  \
-            + u'. Tiedoston maksimikoko on ' \
-            + sizeformat(self.max_image_size) + '.'
+        # If we want to show thumbnail field, set description etc.
+        # Otherwise delete field from form.
+        if self.show_thumbnail_field:
+            # Set thumbnail "field required" flag
+            if self.compo.thumbnail_pref == 0:
+                self.fields['imagefile_original'].required = True
+            else:
+                self.fields['imagefile_original'].required = False
+            
+            # Description for imagefile
+            self.fields['imagefile_original'].help_text = \
+                u"Kuva teokselle. Tätä käytetään mm. arkistossa ja kompomaatin äänestysvaiheessa. Sallitut kuvaformaatit: " \
+                + self.compo.readable_image_formats()  \
+                + u'. Tiedoston maksimikoko on ' \
+                + sizeformat(self.max_image_size) + '.'
+        else:
+            del self.fields['imagefile_original']
 
     class Meta:
         model = Entry
         fields = ('name','creator','description','entryfile','sourcefile','imagefile_original')
 
+    def get_file_ext(self, fname):
+        return os.path.splitext(self.cleaned_data[fname].name)[1][1:]
+
+    def get_file_size(self, fname):
+        return self.cleaned_data[fname].size
+
     def field_size_ok(self, fname, limit):
-        if self.cleaned_data[fname] == None:
-            return True
-        return (self.cleaned_data[fname].size < limit)
+        return (self.get_file_size(fname) <= limit)
 
     def field_format_ok(self, fname, formatfield):
-        if self.cleaned_data[fname] == None:
-            return True
-        allowed = formatfield.split('|')
-        type = os.path.splitext(self.cleaned_data[fname].name)[1][1:]
-        return (type in allowed)
+        return (self.get_file_ext(fname) in formatfield.split('|'))
 
     def clean_entryfile(self):
+        # Check if entryfile is set
+        if self.cleaned_data['entryfile'] == None:
+            return None
+        
+        # Check entry file size
         if not self.field_size_ok("entryfile", self.max_entry_size):
             raise ValidationError(u'Tiedoston koko on liian suuri! Suurin sallittu koko on ' + sizeformat(self.max_entry_size) + '.')
+        
+        # Check entry file format
         if not self.field_format_ok("entryfile", self.compo.formats):
             raise ValidationError(u'Tiedostotyyppi ei ole sallittu. Sallitut formaatit: ' + self.compo.readable_entry_formats() + '.')
+        
+        # All done.
         return self.cleaned_data['entryfile']
     
     def clean_sourcefile(self):
+        # Check if sourcefile is set
+        if self.cleaned_data['sourcefile'] == None:
+            return None
+        
+        # Check source file size
         if not self.field_size_ok("sourcefile", self.max_source_size):
             raise ValidationError(u'Tiedoston koko on liian suuri! Suurin sallittu koko on ' + sizeformat(self.max_source_size) + '.')
+        
+        # Check source file format
         if not self.field_format_ok("sourcefile", self.compo.source_formats):
             raise ValidationError(u'Tiedostotyyppi ei ole sallittu. Sallitut formaatit: ' + self.compo.readable_source_formats() + '.')
+        
+        # All done.
         return self.cleaned_data['sourcefile']
 
     def clean_imagefile_original(self):
+        # Check if imagefile_original is set
         if self.cleaned_data['imagefile_original'] == None:
             return None
         
@@ -265,14 +306,22 @@ class EntryForm(forms.ModelForm):
             raise ValidationError(u'Tiedoston koko on liian suuri! Suurin sallittu koko on ' + sizeformat(self.max_image_size) + '.')
         
         # Check image format
-        type = os.path.splitext(self.cleaned_data["imagefile_original"].name)[1][1:]
-        if not (type in self.imageformats):
-            raise ValidationError(u'Tiedostotyyppi ei ole sallittu. Sallitut formaatit: ' + ', '.join(self.imageformats) + '.')
+        if not self.field_format_ok("imagefile_original", self.compo.image_formats):
+            raise ValidationError(u'Tiedostotyyppi ei ole sallittu. Sallitut formaatit: ' + self.compo.readable_image_formats()  + '.')
         
         # Done
         return self.cleaned_data['imagefile_original']
 
     def validate(self):
+        # Make sure compo is active
         if not self.compo.active:
             raise ValidationError(u'Kompo ei ole aktiivinen.')
-
+        
+    def save(self, commit=True):
+        instance = super(EntryForm, self).save(commit=False)
+        if self.compo.thumbnail_pref == 1:
+            name = unicode('gth_'+os.path.basename(instance.entryfile.url))
+            instance.imagefile_original.save(name, instance.entryfile, commit)
+        if commit:
+            instance.save()
+        return instance

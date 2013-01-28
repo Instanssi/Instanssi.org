0615ff6a900b938cfc5583238b50dacb54dcc03b
Tuomas Virtanen <katajakasa@gmail.com>
2013-01-28 23:16:46 +0200

    tickets: Remove unnecessary url handler

--- Instanssi/tickets/urls.py
+++ Instanssi/tickets/urls.py
@@ -4,7 +4,6 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns(
     'Instanssi.tickets.views',
-    url(r'^$', 'index', name="index"),
     url(r'^info/(?P<ticket_key>\w+)/', 'ticket', name="ticket"),
     url(r'^ta/(?P<transaction_key>\w+)/', 'tickets', name="tickets"),
 )

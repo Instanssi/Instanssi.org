36f14a6ea37d4e438deeaa5f7c5813eaa1ecd114
Janne Leppänen <janne-leppanen@hotmail.com>
2013-01-30 21:38:24 +0200

    Instanssi 2013: Store layout update.

--- Instanssi/main2013/static/main2013/js/functions.js
+++ Instanssi/main2013/static/main2013/js/functions.js
@@ -30,5 +30,41 @@ $(document).ready(function (){
 
 		$( '#countdown' ).html( daysLeft + " päivää, " + hrsLeft +" tuntia, " + minsLeft + " minuuttia" )
 	}
+
+	/* store +/- buttons */
+	
+	/* add +/- buttons */
+	$( '.store-items input' ).each( function() {
+		$( this ).after('<div class="inc-dec-btn" ><div>+</div><div>-</div></div>');
+	});
+
+	/* increase and decrease buttton logic  */ 
+	$(".store-items .inc-dec-btn div").click(function() {
+	    var $button = $( this );
+	    var oldValue = $button.parent().parent().find( "input" ).val();
+	    var newVal = oldValue;
+
+	    if ( $button.text() == "+" ) {
+		 	var newVal = parseFloat( oldValue ) + 1;
+		} else {
+		  	// Don't allow decrementing below zero
+		  	if ( oldValue >= 1 ) {
+		     	var newVal = parseFloat( oldValue ) - 1;
+		  	}
+		}
+		$button.parent().parent().find( "input" ).val( newVal );
+		storeSum();
+	});
+
+	/* count money sum of store items  */
+	function storeSum(){
+		var  storeSum = 0;
+		$( ".store-items input" ).each( function() {
+			// amount * price
+			storeSum += parseFloat( $( this ).val() ) * parseFloat( $( this ).parent().parent().find('.item-price').html() )
+		});
+		$('.store .store-sum').html( storeSum.toFixed(2) );
+	}
+
 	
 });

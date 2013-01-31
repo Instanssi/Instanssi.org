e352fc3f661c5bdcdc5e804d6a0fca2f9cc9f401
Tuomas Virtanen <katajakasa@gmail.com>
2013-01-31 15:00:06 +0200

    Cleanups

--- Instanssi/store/forms.py
+++ Instanssi/store/forms.py
@@ -57,7 +57,8 @@ class StoreOrderForm(forms.ModelForm):
             
             if item.imagefile_thumbnail:
                 item_fields.fields.append(
-                    HTML('<img class="item-image" src="%s" width="64" height="64" alt="Tuotakuva" data-bigimg="%s" />' % (item.imagefile_thumbnail.url, item.imagefile_original.url)),
+                    HTML('<img class="item-image" src="%s" width="64" height="64" alt="Tuotakuva" data-bigimg="%s" />' 
+                         % (item.imagefile_thumbnail.url, item.imagefile_original.url)),
                 )
 
             item_fields.fields.append(

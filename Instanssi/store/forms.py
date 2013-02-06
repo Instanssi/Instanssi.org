8357d96cf222a7aaaf7aa3afabe24ef1f50664f9
Tuomas Virtanen <katajakasa@gmail.com>
2013-02-06 23:43:17 +0200

    store: Errh.

--- Instanssi/store/forms.py
+++ Instanssi/store/forms.py
@@ -181,4 +181,4 @@ class StoreOrderForm(forms.ModelForm):
 
     class Meta:
         model = StoreTransaction
-        exclude = ('time_created', 'time_paid', 'token', 'paid', 'key', 'store')
+        exclude = ('time_created', 'time_paid', 'token', 'paid', 'key', 'status')

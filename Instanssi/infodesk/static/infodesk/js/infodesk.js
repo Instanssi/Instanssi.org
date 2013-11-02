886840c3efe9cede60280622d320eef449c53495
Tommi Teistela <totateis@gmail.com>
2013-11-02 20:04:43 +0200

    Add Select2 for future UI improvements

---
+++ Instanssi/infodesk/static/infodesk/js/infodesk.js
@@ -0,0 +1,31 @@
+/*jslint indent: 4 */
+$(function () {
+    // Set up Select2 for infodesk forms
+    $("#lookup").select2({
+        placeholder: "Kirjoita osa haettavasta nimestä",
+        minimumInputLength: 2,
+        ajax: {
+            url: "ticket_lookup_ac",
+            datatype: "json",
+            data: function (term, page) {
+                return {
+                    term: term
+                };
+            },
+            results: function (data, page) {
+                var results = [];
+                $.each(data, function () {
+                    results.push({
+                        id: this,
+                        text: this
+                    });
+                });
+                return {
+                    results: results,
+                    context: null,
+                    more: false
+                };
+            }
+        }
+    });
+});

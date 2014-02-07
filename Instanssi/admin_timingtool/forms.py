101966911593558fb0977dd86862fcc3206efbeb
Tuomas Virtanen <katajakasa@gmail.com>
2014-02-07 23:15:10 +0200

    admin_timingtool: Remove. Old and deprecated code.

--- Instanssi/admin_timingtool/forms.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django import forms
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit, Layout, Fieldset, ButtonHolder
-
-class TimingForm(forms.Form):
-    moveby = forms.TimeField(label=u'Kuinka paljon tapahtumia siirretään')
-    direction = forms.ChoiceField(label=u'Siirron suunta', choices=[(0, u'Taaksepäin'), (1, u'Eteenpäin')])
--- Instanssi/admin_timingtool/urls.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns(
-    'Instanssi.admin_timingtool.views',
-    url(r'^$', 'index', name="index"),
-)

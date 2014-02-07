419baedbb5022d1391ab862494861919efe8ac57
Tuomas Virtanen <katajakasa@gmail.com>
2014-02-08 00:14:02 +0200

    kompomaatti_eventselect: Remove as unnecessary

--- Instanssi/kompomaatti_eventselect/urls.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns(
-    'Instanssi.kompomaatti_eventselect.views',
-    url(r'^$', 'index', name="index"),
-)

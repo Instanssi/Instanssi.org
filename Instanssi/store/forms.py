afe64fc66284ceda1741148d1d6fefd0cedd3107
Tuomas Virtanen <katajakasa@gmail.com>
2014-02-16 03:38:40 +0200

    store: Fixed store forms

--- Instanssi/store/forms.py
+++ Instanssi/store/forms.py
@@ -9,10 +9,9 @@ import logging
 from django import forms
 from django.core.urlresolvers import reverse
 from django.db import IntegrityError
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import \
-    Submit, Layout, Fieldset, ButtonHolder, Hidden, Div, HTML
-from Instanssi.store.models import StoreItem, TransactionItem, StoreTransaction
+from django.utils.html import format_html
+
+from Instanssi.store.models import StoreTransaction, TransactionItem, StoreItem
 
 # Logger
 logger = logging.getLogger(__name__)
@@ -23,52 +22,66 @@ def gen_sha(text):
     h.update(text)
     return h.hexdigest()
 
+class ProductWidget(forms.NumberInput):
+    def __init__(self, attrs=None):
+        self.large_image = attrs.pop('large_image')
+        self.small_image = attrs.pop('small_image')
+        self.price = attrs.pop('price')
+        self.available = attrs.pop('available')
+        super(ProductWidget, self).__init__(attrs)
+    
+    def render(self, name, value, attrs=None):
+        num_field = super(ProductWidget, self).render(name, value, attrs)
+        
+        # Form HTML for sold out field
+        available_html = u''
+        if self.available <= 0:
+            available_html = u'<div class="item-soldout">(Lopussa)</div>\n'
+        
+        # Form final HTML
+        return format_html('\n\
+            <div class="item-image">\n\
+                <a class="item-fancybox" href="{0}">\n\
+                    <img src="{1}" width="64" height="64" alt="Tuotekuva" />\n\
+                </a>\n\
+            </div>\n\
+            <div class="item-field">{2}</div>\n\
+            <div class="item-price">{3} €/kpl</div>\n\
+            {4}', 
+            self.large_image,  
+            self.small_image, 
+            num_field, 
+            self.price, 
+            available_html
+        )
+    
 class StoreProductsForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super(StoreProductsForm, self).__init__(*args, **kwargs)
-        self.helper = FormHelper()
 
-        item_fields = Fieldset(u'', css_class='store-items')
         for item in StoreItem.items_available():
             name = 'item-%s' % item.id
             self.fields[name] = forms.IntegerField(
-                initial=0, min_value=0, max_value=item.num_available(),
+                widget=ProductWidget(attrs={
+                    'small_image': item.imagefile_thumbnail.url,
+                    'large_image': item.imagefile_original.url,
+                    'available': item.num_in_store(),
+                    'price': item.price,
+                }),
+                initial=0, 
+                min_value=0, 
+                max_value=item.num_available(),
                 label=u'%s' % (item.name),
                 help_text=item.description, 
                 required=False
             )
             
-            # Set data-maxvalue attr for field, just for javascript stuff
-            self.fields[name].widget.attrs['data-maxvalue'] = item.num_available()
-            
-            # Container
-            mdiv = Div()
-            
-            # Print message if item is sold out
-            if item.num_in_store() <= 0:
-                self.fields[name].widget.attrs['disabled'] = True
-                self.fields[name].label += u' <span class="item-soldout">(Lopussa)</span>'
-            
-            # Print img tag if item has image
-            if item.imagefile_thumbnail:
-                mdiv.fields.append(
-                    HTML('<a class="item-image fancybox" href="%s"><img src="%s" width="64" height="64" alt="Tuotekuva" /></a>' 
-                         % (item.imagefile_original.url, item.imagefile_thumbnail.url)),
-                )
-
-            mdiv.fields.append(
-                HTML(u'<span class="item-price">%d €/kpl</span>' % item.price)
-            )
-            mdiv.fields.append(name)
-            item_fields.fields.append(mdiv)
-            
-        self.helper.layout = Layout(item_fields,)
-        
     def _dataitems(self):
         for key, value in self.data.iteritems():
             try:
-                if key.startswith('item-') and int(value):
-                    yield (key[5:], int(value))
+                pos = key.rfind('item-') + 5
+                if pos > -1 and int(value):
+                    yield (key[pos:], int(value))
             except:
                 continue
 
@@ -78,7 +91,7 @@ class StoreProductsForm(forms.Form):
         fails = []
 
         # also check that the purchase amount for each field makes sense
-        for (item_id, amount) in self._dataitems():
+        for item_id, amount in self._dataitems():
             store_item = StoreItem.objects.get(id=int(item_id))
             total_items += amount
             if store_item.num_available() < amount:
@@ -100,46 +113,28 @@ class StoreProductsForm(forms.Form):
 
 class StoreInfoForm(forms.ModelForm):
     email_confirm = forms.EmailField(
-        label=u'Vahvista sähköposti', max_length=255
+        label=u'Vahvista sähköposti', 
+        max_length=255
     )
 
     def __init__(self, *args, **kwargs):
         super(StoreInfoForm, self).__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.layout = Layout(
-            Fieldset(
-                u'',
-                'firstname',
-                'lastname',
-                'email',
-                'email_confirm',
-                'telephone',
-                'mobile',
-                'company',
-                'street',
-                'postalcode',
-                'city',
-                'country',
-                'information',
-            )
-        )
 
     def clean(self):
         cleaned_data = super(StoreInfoForm, self).clean()
         fails = []
 
         # Make sure the email fields match
-        if 'email' in self.data and 'email_confirm' in self.data:
-            if self.data['email'] != self.data['email_confirm']:
-                if not 'email_confirm' in self._errors:
-                    self._errors['email_confirm'] = self.error_class()
-                self._errors['email_confirm'].append(
-                    u'Osoitteet eivät täsmää!'
-                )
-                fails.append(
-                    u'Vahvista sähköpostiosoitteesi kirjoittamalla sama '
-                    u'osoite molempiin kenttiin!'
-                )
+        if cleaned_data['email'] != cleaned_data['email_confirm']:
+            if not 'email_confirm' in self._errors:
+                self._errors['email_confirm'] = self.error_class()
+            self._errors['email_confirm'].append(
+                u'Osoitteet eivät täsmää!'
+            )
+            fails.append(
+                u'Vahvista sähköpostiosoitteesi kirjoittamalla sama '
+                u'osoite molempiin kenttiin!'
+            )
 
         # Dump errors
         if fails:
@@ -150,24 +145,18 @@ class StoreInfoForm(forms.ModelForm):
 
     class Meta:
         model = StoreTransaction
-        exclude = ('time_created', 'time_paid', 'token', 'paid', 'key', 'status')
+        fields = ('firstname', 'lastname', 'email', 'telephone', 'mobile', 'company', 'street', 'postalcode', 'city', 'country', 'information')
         
 class StorePaymentMethodForm(forms.Form):
     read_terms = forms.BooleanField(
-        label=u'Hyväksyn toimitusehdot', required=True
+        label=u'Hyväksyn toimitusehdot', 
+        required=True,
     )
 
     def __init__(self, *args, **kwargs):
         super(StorePaymentMethodForm, self).__init__(*args, **kwargs)
-        self.helper = FormHelper()
         
         self.fields['read_terms'].help_text = \
             u'Olen lukenut <a href="%s" target="_blank">toimitusehdot</a> ' \
             u'ja hyväksyn ne. (Luethan myös <a href="%s" target="_blank">rekisteriselosteen</a>)' % (reverse('store:terms'), reverse('store:privacy'))
 
-        self.helper.layout = Layout(
-            Fieldset(
-                u'Maksutavan valinta',
-                'read_terms',
-            )
-        )

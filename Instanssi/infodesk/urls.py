a5af8847ad5c684c75439f9f23e2c83eacc7e3c1
Tuomas Virtanen <katajakasa@gmail.com>
2014-02-17 08:03:39 +0200

    infodesk: Make it work again

--- Instanssi/infodesk/urls.py
+++ Instanssi/infodesk/urls.py
@@ -4,13 +4,12 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     'Instanssi.infodesk.views',
-    url(r'^(?P<event_id>\d+)/$', 'index', name="index"),
-    url(r'^(?P<event_id>\d+)/ticket/check/', 'ticket_check', name="ticket_check"),
-    url(r'^(?P<event_id>\d+)/store/check/', 'store_check', name="store_check"),
-    url(r'^(?P<event_id>\d+)/ticket/info/(?P<ticket_id>\d+)/', 'ticket_info', name="ticket_info"),
-    url(r'^(?P<event_id>\d+)/store/info/(?P<transaction_id>\d+)/', 'store_info', name="store_info"),
-    url(r'^(?P<event_id>\d+)/ticket/mark/(?P<ticket_id>\d+)/', 'ticket_mark', name="mark_ticket"),
-    url(r'^(?P<event_id>\d+)/store/mark/(?P<transaction_id>\d+)/', 'store_mark', name="mark_transaction"),
-    url(r'^(?P<event_id>\d+)/ticket_lookup_ac', 'ticket_lookup_autocomplete', name="ticket_lookup_autocomplete"),
-    url(r'^(?P<event_id>\d+)/ticket_lookup', 'ticket_lookup', name="ticket_lookup"),
+    url(r'^$', 'index', name="index"),
+    url(r'^item/check/', 'item_check', name="item_check"),
+    url(r'^transaction/check/', 'transaction_check', name="transaction_check"),
+    url(r'^item/info/(?P<item_id>\d+)/', 'item_info', name="item_info"),
+    url(r'^transaction/info/(?P<transaction_id>\d+)/', 'transaction_info', name="transaction_info"),
+    url(r'^item/mark/(?P<item_id>\d+)/', 'item_mark', name="item_mark"),
+    url(r'^ta_lookup_ac', 'ta_lookup_autocomplete', name="ta_lookup_autocomplete"),
+    url(r'^ta_lookup', 'ta_lookup', name="ta_lookup"),
 )
--- Instanssi/infodesk/views.py
+++ Instanssi/infodesk/views.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from common.responses import JSONResponse
+from datetime import datetime
+
 from django.db.models import Q
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect,Http404
 from django.core.urlresolvers import reverse
+
+from common.responses import JSONResponse
 from Instanssi.infodesk.misc.auth_decorator import infodesk_access_required
-from Instanssi.infodesk.forms import TransactionKeyScanForm, TicketKeyScanForm
-from Instanssi.tickets.models import Ticket
+from Instanssi.infodesk.forms import TransactionKeyScanForm, ItemKeyScanForm
 from Instanssi.store.models import StoreTransaction, TransactionItem
-from Instanssi.kompomaatti.models import Event
 
 # Logging related
 import logging
@@ -18,16 +19,15 @@ logger = logging.getLogger(__name__)
 
 
 @infodesk_access_required
-def index(request, event_id):
-    event = get_object_or_404(Event, pk=event_id)
-    return render_to_response('infodesk/index.html', {
-        'event': event
-    }, context_instance=RequestContext(request))
-
+def index(request):
+    return render_to_response('infodesk/index.html', {}, context_instance=RequestContext(request))
 
 @infodesk_access_required
-def ticket_lookup_autocomplete(request, event_id):
-    lookup = request.GET['term']
+def ta_lookup_autocomplete(request):
+    try:
+        lookup = request.GET['term']
+    except:
+        raise Http404
 
     # lookup name in people who have bought something
     txns = StoreTransaction.objects.filter(
@@ -36,8 +36,7 @@ def ticket_lookup_autocomplete(request, event_id):
 
 
 @infodesk_access_required
-def ticket_lookup(request, event_id):
-    event = get_object_or_404(Event, pk=event_id)
+def ta_lookup(request):
     # The form autocompletes names like "lastname, firstname"
     # If somebody wrote a comma in their name, they can buy a new ticket.
     # We could also accept ids (with some potential UI problems).
@@ -50,99 +49,68 @@ def ticket_lookup(request, event_id):
         lastname = name[0]
         firstname = name[1].lstrip()
     except:
-        pass
+        raise Http404
 
     storetransactions = StoreTransaction.objects.filter(
         lastname=lastname,
         firstname=firstname
     )
 
-    transactions = []
-    for transaction in storetransactions:
-        transactions.append({
-            'id': transaction.id,
-            'created': transaction.time_created,
-            'status': transaction.get_status_display,
-            'tickets': Ticket.objects.filter(transaction=transaction),
-            'others': TransactionItem.objects.filter(
-                transaction=transaction).exclude(item__delivery_type=1)
-        })
-
-    return render_to_response('infodesk/ticket_lookup.html', {
-        'Ticket': Ticket,
-        'event': event,
-        'transactions': transactions,
+    return render_to_response('infodesk/ta_lookup.html', {
+        'transactions': storetransactions,
         'name': names
     }, context_instance=RequestContext(request))
 
 
 @infodesk_access_required
-def ticket_check(request, event_id):
-    event = get_object_or_404(Event, pk=event_id)
-
+def item_check(request):
     if request.method == 'POST':
-        form = TicketKeyScanForm(request.POST, event=event)
+        form = ItemKeyScanForm(request.POST)
         if form.is_valid():
-            return HttpResponseRedirect(reverse('infodesk:ticket_info', args=(event.id, form.ticket.id,)))
+            return HttpResponseRedirect(reverse('infodesk:item_info', args=(form.item.id,)))
     else:
-        form = TicketKeyScanForm(event=event)
+        form = ItemKeyScanForm()
 
-    return render_to_response('infodesk/ticket_check.html', {
-        'event': event,
+    return render_to_response('infodesk/item_check.html', {
         'form': form
     }, context_instance=RequestContext(request))
 
 
 @infodesk_access_required
-def store_check(request, event_id):
-    event = get_object_or_404(Event, pk=event_id)
-
+def transaction_check(request):
     if request.method == 'POST':
         form = TransactionKeyScanForm(request.POST)
         if form.is_valid():
-            return HttpResponseRedirect(reverse('infodesk:store_info', args=(event.id, form.transaction.id,)))
+            return HttpResponseRedirect(reverse('infodesk:transaction_info', args=(form.transaction.id,)))
     else:
         form = TransactionKeyScanForm()
 
-    return render_to_response('infodesk/store_check.html', {
-        'event': event,
+    return render_to_response('infodesk/ta_check.html', {
         'form': form
     }, context_instance=RequestContext(request))
 
 
 @infodesk_access_required
-def ticket_mark(request, event_id, ticket_id):
-    ticket = get_object_or_404(Ticket, pk=ticket_id)
-    ticket.used = True
-    ticket.save()
-    logger.info('Ticket %d marked as used.' % (ticket.id,), extra={'user': request.user, 'event_id': event_id})
-    return HttpResponseRedirect(reverse('infodesk:index', args=(event_id,)))
+def item_mark(request, item_id):
+    item = get_object_or_404(TransactionItem, pk=item_id)
+    if item.transaction.is_paid:
+        item.time_delivered = datetime.now()
+        item.save()
+        logger.info('Item %d marked as delivered.' % (item.id,), extra={'user': request.user})
+    return HttpResponseRedirect(reverse('infodesk:index'))
 
 
 @infodesk_access_required
-def store_mark(request, event_id, transaction_id):
-    ta = get_object_or_404(StoreTransaction, pk=transaction_id)
-    ta.status = 2
-    ta.save()
-    logger.info('Transaction %d marked as delivered.' % (ta.id,), extra={'user': request.user, 'event_id': event_id})
-    return HttpResponseRedirect(reverse('infodesk:index', args=(event_id,)))
-
-
-@infodesk_access_required
-def ticket_info(request, event_id, ticket_id):
-    event = get_object_or_404(Event, pk=event_id)
-    ticket = get_object_or_404(Ticket, pk=ticket_id)
-    return render_to_response('infodesk/ticket_info.html', {
-        'event': event,
-        'ticket': ticket
+def item_info(request, item_id):
+    item = get_object_or_404(TransactionItem, pk=item_id)
+    return render_to_response('infodesk/item_info.html', {
+        'item': item
     }, context_instance=RequestContext(request))
 
 
 @infodesk_access_required
-def store_info(request, event_id, transaction_id):
-    event = get_object_or_404(Event, pk=event_id)
+def transaction_info(request, transaction_id):
     transaction = get_object_or_404(StoreTransaction, pk=transaction_id)
-    return render_to_response('infodesk/store_info.html', {
-        'event': event,
+    return render_to_response('infodesk/ta_info.html', {
         'transaction': transaction
     }, context_instance=RequestContext(request))

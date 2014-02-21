dad79afdb31399339ba33bc72bd4645b0ad46a46
Tuomas Virtanen <katajakasa@gmail.com>
2014-02-21 07:45:55 +0200

    admin_base: Redirect to users:login if necessary

--- Instanssi/admin_base/misc/auth_decorator.py
+++ Instanssi/admin_base/misc/auth_decorator.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from django.conf import settings
 from django.http import HttpResponseRedirect
 from common.http import Http403
+from django.core.urlresolvers import reverse
 
 def staff_access_required(view_func):
     def _checklogin(request, *args, **kwargs):
@@ -10,7 +10,7 @@ def staff_access_required(view_func):
             if request.user.is_staff:
                 return view_func(request, *args, **kwargs)
             raise Http403
-        return HttpResponseRedirect(getattr(settings, 'ADMIN_LOGIN_URL')) 
+        return HttpResponseRedirect(reverse('users:login')) 
     return _checklogin
 
 def su_access_required(view_func):
@@ -19,5 +19,5 @@ def su_access_required(view_func):
             if request.user.is_staff and request.user.is_superuser:
                 return view_func(request, *args, **kwargs)
             raise Http403
-        return HttpResponseRedirect(getattr(settings, 'ADMIN_LOGIN_URL')) 
+        return HttpResponseRedirect(reverse('users:login')) 
     return _checklogin

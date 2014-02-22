d5c224abe9a96c7ce157d3763cb377eb21a844a0
Tuomas Virtanen <katajakasa@gmail.com>
2014-02-22 18:20:00 +0200

    Remove old auth decorator files, and move all the contens to common.auth

--- common/auth.py
+++ common/auth.py
@@ -28,3 +28,12 @@ def su_access_required(view_func):
             raise Http403
         return HttpResponseRedirect(reverse('users:login')+'?next='+request.get_full_path()) 
     return _checklogin
+
+def infodesk_access_required(view_func):
+    def _checklogin(request, *args, **kwargs):
+        if request.user.is_authenticated() and request.user.is_active:
+            if request.user.has_perm('store.change_storetransaction'):
+                return view_func(request, *args, **kwargs)
+            raise Http403
+        return HttpResponseRedirect(reverse('users:login')+'?next='+request.get_full_path()) 
+    return _checklogin

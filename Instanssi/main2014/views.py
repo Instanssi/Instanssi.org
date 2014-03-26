de85253c7ad48f670324561f88893fb46534a97d
Tuomas Virtanen <katajakasa@gmail.com>
2014-03-27 01:47:54 +0200

    main2014: Add missing import

--- Instanssi/main2014/views.py
+++ Instanssi/main2014/views.py
@@ -3,6 +3,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from Instanssi.main2014.models import ToimistoJahti,ToimistoSuoritus
 from Instanssi.main2014.forms import ToimistoCodeForm

e43b91412ee0899bb6b851a760dd06bce263d099
Tommi Teistelä <totateis@gmail.com>
2015-03-26 17:14:18 +0200

    ext_blog: Allow customizing number of posts displayed

--- Instanssi/ext_blog/templatetags/blog_tags.py
+++ Instanssi/ext_blog/templatetags/blog_tags.py
@@ -7,8 +7,8 @@ from Instanssi.ext_blog.models import BlogEntry
 register = template.Library()
 
 @register.inclusion_tag('ext_blog/blog_messages.html')
-def render_blog(event_id):
-    entries = BlogEntry.objects.filter(event_id__lte=int(event_id), public=True).order_by('-date')[:10]
+def render_blog(event_id, max_posts=10):
+    entries = BlogEntry.objects.filter(event_id__lte=int(event_id), public=True).order_by('-date')[:max_posts]
     return {'entries': entries}
 
 @register.inclusion_tag('ext_blog/blog_rss_tag.html')

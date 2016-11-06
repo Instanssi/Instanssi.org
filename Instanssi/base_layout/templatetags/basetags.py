15fdfffee9a62e03f33d0c737def904e0d1f3d88
Tuomas Virtanen <katajakasa@gmail.com>
2016-11-06 18:17:11 +0200

    Fix gravatar tag

--- Instanssi/base_layout/templatetags/basetags.py
+++ Instanssi/base_layout/templatetags/basetags.py
@@ -8,4 +8,4 @@ register = template.Library()
 
 @register.simple_tag
 def gravatar_url(email):
-    return 'https://www.gravatar.com/avatar/{}?d=retro'.format(hashlib.md5(email.lower()).hexdigest())
+    return 'https://www.gravatar.com/avatar/{}?d=retro'.format(hashlib.md5(email.lower().encode('UTF-8')).hexdigest())

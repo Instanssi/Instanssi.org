2b0837b89a47b9f2a147cb4c674b7f9639116a89
Tommi Teistela <totateis@gmail.com>
2016-11-21 00:51:26 +0200

    main2017: Add animated background layer (again)
    Layer image needs work, but JS works even with media queries.

--- Instanssi/main2017/static/main2017/js/base.js
+++ Instanssi/main2017/static/main2017/js/base.js
@@ -1,3 +1,27 @@
 $(function() {
     // layout-specific JS can go here.
+    var $layer0 = $('.header-bg-0');
+    var $layer1 = $('.header-bg-1');
+    var $window = $(window);
+
+    function moveLayers(offset) {
+        var ww = $window.width();
+        var base = ww / 2; // center pattern by default
+        if(ww >= 1200) {
+            base += 230;
+        } else if (ww >= 980) {
+            base += 230 + 65;
+        } else {
+            base += 230 + 140;
+        }
+        var pos = base + offset / 100;
+        $layer1.css('background-position-x', pos);
+        // TODO: Cubes!
+    }
+    $('body').mousemove(function(ev) {
+        // TODO: Make this smoother! Keep track of the current and target offset, and animate it until we reach our goal.
+        // (use requestframe if available?)
+        moveLayers($window.width() / 2 - ev.pageX);
+    });
+    moveLayers(0);
 });

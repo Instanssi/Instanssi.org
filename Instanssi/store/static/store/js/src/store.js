1ee311bf98fc4a0ad8614addcbb435cf5f31e069
Tommi Teistela <totateis@gmail.com>
2016-12-08 23:31:49 +0200

    store: Replace order UI with a single page app

---
+++ Instanssi/store/static/store/js/src/store.js
@@ -0,0 +1,252 @@
+/* global Vue2 */
+
+toLocaleStringSupportsOptions = !!(typeof Intl == 'object' && Intl && typeof Intl.NumberFormat == 'function');
+
+function formatPriceLocaleString(price) {
+    return price.toLocaleString('fi', { style: 'currency', currency: 'EUR'});
+}
+function formatPriceLegacy(price) {
+    return price.toFixed(2) + ' €';
+}
+let formatPrice;
+if(toLocaleStringSupportsOptions) {
+    formatPrice = formatPriceLocaleString;
+} else {
+    formatPrice = formatPriceLegacy;
+}
+
+
+Vue.component('store-product', {
+    props: {
+        product: Object
+    },
+    data: function () {
+        return {
+            variant: null,
+        };
+    },
+    methods: {
+        addItem: function() {
+            this.$emit('addItem', this.product, this.variant);
+        }
+    },
+    created: function () {
+        var product = this.product;
+        if (product.variants && product.variants.length) {
+            this.variant = product.variants[0];
+        }
+    },
+    template: `
+    <li class="product">
+        <div class="pull-left">
+            <img :src="product.imagefile_thumbnail_url" width="48" height="48" />
+            {{ product.name }} ({{ product.price.toFixed(0) }} €)
+            <p class="small" v-if="product.description" v-html="product.description"></p>
+            <p v-if="product.qtyDiscountThresh">
+                <span class="fa fa-check"/>
+                Buy at least {{ product.qtyDiscountThresh }} and earn a discount!
+            </p>
+        </div>
+        <span class="pull-right">
+            <select v-if="product.variants" v-model="variant">
+                <option v-for="variant in product.variants" v-bind:value="variant">
+                    {{ variant.title }}
+                </option>
+            </select>
+            <button v-on:click="addItem()">Lisää <span class="fa fa-shopping-cart"/></button>
+        </span>
+        <span class="clearfix"></span>
+    </li>
+    `,
+});
+
+function storeXHR(method, path, data) {
+    return new Promise((resolve, reject) => {
+        var xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            resolve(JSON.parse(xhr.responseText));
+        };
+        xhr.onerror = function () {
+            reject(xhr.responseText ? JSON.parse(xhr.responseText) : null);
+        };
+        xhr.open(method, path);
+        if (data) {
+            xhr.data = JSON.stringify(data);
+        }
+        xhr.send();
+    });
+}
+
+var app = new Vue({
+    el: '#store',
+    data: {
+        products: [{
+            id: 1,
+            title: 'Awesome T-shirt',
+            description: 'Instanssi, represent',
+            price: 20.00,
+            variants: [{
+                id: 1,
+                title: 'Size S'
+            }, {
+                id: 2,
+                title: 'Size M'
+            }, {
+                id: 3,
+                title: 'Size L'
+            }, {
+                id: 4,
+                title: 'Size XL'
+            }]
+        }, {
+            id: 2,
+            title: 'Basic ticket',
+            description: 'Gets you through the doors',
+            price: 15.00,
+            qtyDiscountThresh: 5,
+            qtyDiscountFactor: .9
+        }, {
+            id: 3,
+            title: 'Elite ticket',
+            description: 'Gets you some more stuff (or not)',
+            price: 50.00,
+        }],//*/
+        items: [],
+        totalPrice: 0
+    },
+    template: `
+<div>
+<h3>Tuotteet</h3>
+<ul class="list-unstyled store-items">
+    <store-product v-for="product in products" :product="product"
+        v-on:addItem="addItem" />
+</ul>
+<h3>Ostoskori</h3>
+<ul class="list-unstyled store-items">
+    <li v-for="item in items">
+        <div class="pull-left">{{ item.product.name }}
+            <span v-if="item.variant">({{ item.variant.name }})</span>
+        </div>
+        <span class="pull-right">
+            x {{ item.count }} = {{ getSubtotal(item) }} €
+            <button v-on:click="changeItemCount(item, 1)">+</button>
+            <button v-on:click="changeItemCount(item, -1)">-</button>
+            <button v-on:click="removeItem(item)">
+                <span class="fa fa-fw fa-trash"></span>
+            </button>
+        </span>
+        <span class="clearfix"></span>
+    </li>
+    <li>Yhteensä: {{ totalPrice.toFixed(2) }} €</li>
+</ul>
+<h3>Tiedot</h3>
+<form class="form-horizontal">
+    <p>Huomaathan, että lippukoodit lähetetään annettuun sähköpostiosoitteeseen.</p>
+    <div class="form-group">
+        <label class="col-sm-4 control-label">Nimi</label>
+        <div class="col-sm-8">
+            <input class="form-control"></input>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-4 control-label">Sähköposti</label>
+        <div class="col-sm-8">
+            <input class="form-control"></input>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-4 control-label">Katuosoite</label>
+        <div class="col-sm-8">
+            <input class="form-control"></input>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-4 control-label">Postinumero</label>
+        <div class="col-sm-8">
+            <input class="form-control"></input>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-4 control-label">Kaupunki</label>
+        <div class="col-sm-8">
+            <input class="form-control"></input>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-4 control-label">Puhelin</label>
+        <div class="col-sm-8">
+            <input class="form-control"></input>
+        </div>
+    </div>
+    <span class="pull-right">
+        <button class="btn btn-primary">Jatka &gt;</button>
+    </span>
+    <div class="clearfix"></div>
+</form>
+</div>
+  `,
+    created: function() {
+        storeXHR('GET', '/api/store_items/?format=json').then((items) => {
+            items.forEach((item) => {
+                item.price = parseFloat(item.price);
+            });
+            this.products = items;
+        }).catch((e) => {
+            console.error('error fetching store items:', e);
+        });
+    },
+    methods: {
+        getSubtotal: function(item) {
+            var multiplier = 1;
+            var qtyThresh = item.product.qtyDiscountThresh;
+            var discountFactor = item.product.qtyDiscountFactor;
+            if(qtyThresh && item.count >= qtyThresh) {
+                multiplier *= discountFactor;
+            }
+            return item.product.price * item.count * multiplier;
+        },
+        updateTotal: function () {
+            var totalPrice = 0;
+            var component = this;
+            this.items.forEach(function (item) {
+                totalPrice += component.getSubtotal(item);
+            });
+            this.totalPrice = totalPrice;
+        },
+        addItem: function (product, variant) {
+            // check if product/variant is already in items
+            var found = this.items.findIndex(function (item) {
+                return item.product.id === product.id && (!variant || (item.variant && (variant.id === item.variant.id)));
+            });
+            if (found >= 0) {
+                // if it is, splice a new item with a higher count
+                var newItem = Object.assign({}, this.items[found]);
+                newItem.count++;
+                this.items.splice(found, 1, newItem);
+            } else {
+                // if it isn't, push a new item
+                this.items.push({
+                    count: 1,
+                    product: product,
+                    variant: variant
+                });
+            }
+            this.updateTotal();
+        },
+        removeItem: function (item) {
+            var pos = this.items.indexOf(item);
+            this.items.splice(pos, 1);
+            this.updateTotal();
+        },
+        changeItemCount: function (item, change) {
+            var pos = this.items.indexOf(item);
+            var item = this.items[pos];
+            if (item.count + change <= 0) {
+                return this.removeItem(item);
+            }
+            item.count += change;
+            this.items.splice(pos, 1, item);
+            this.updateTotal();
+        }
+    }
+});

80abc02f1085f61cc794431b1f2b2e6a1493c2c8
Tuomas Virtanen <katajakasa@gmail.com>
2016-12-21 21:04:27 +0200

    store: Add StoreItemVariant to admin.py

--- Instanssi/store/admin.py
+++ Instanssi/store/admin.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
-from Instanssi.store.models import StoreItem, StoreTransaction, TransactionItem
+from Instanssi.store.models import StoreItem, StoreTransaction, TransactionItem, StoreItemVariant
 
 
 admin.site.register(StoreItem)
 admin.site.register(StoreTransaction)
 admin.site.register(TransactionItem)
+admin.site.register(StoreItemVariant)

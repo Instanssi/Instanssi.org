ccb6f0e9f55d940c5f8ee84720c4b178d6e457e0
Tuomas Virtanen <katajakasa@gmail.com>
2018-07-11 20:58:14 +0300

    Clear old migrations, make new initial migrations

--- Instanssi/main2014/views.py
+++ Instanssi/main2014/views.py
@@ -1,66 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render
-from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404
-from django.utils import timezone
-
-from Instanssi.main2014.models import ToimistoJahti, ToimistoSuoritus
-from Instanssi.main2014.forms import ToimistoCodeForm
-from Instanssi.common.auth import user_access_required
 
 
 def pageloader(request, templatename):
     return render(request, 'main2014/'+templatename+'.html', {
         'event_id': 8, 
         'templatename': templatename,
-    })
-
-
-def jahti(request, hint_id):
-    hint = get_object_or_404(ToimistoJahti, key=hint_id)
-    
-    return render(request, 'main2014/toimisto/jahti.html', {
-        'event_id': 8,
-        'hint': hint.help,
-    })
-
-
-@user_access_required
-def reportointi(request):
-    is_done = True
-    try:
-        ToimistoSuoritus.objects.get(user=request.user)
-    except ToimistoSuoritus.DoesNotExist:
-        is_done = False
-    
-    if request.method == 'POST' and is_done == False:
-        codeform = ToimistoCodeForm(request.POST)
-        if codeform.is_valid():
-            s = ToimistoSuoritus()
-            s.user = request.user
-            s.nick = codeform.cleaned_data['nick']
-            s.time = timezone.now()
-            s.save()
-            return HttpResponseRedirect(reverse('main2014:toimisto-kiitos'))
-    else:
-        codeform = ToimistoCodeForm()
-    
-    return render(request, 'main2014/toimisto/reportointi.html', {
-        'event_id': 8,
-        'form': codeform,
-        'is_done': is_done,
-    })
-
-
-@user_access_required
-def kiitos(request):
-    par = get_object_or_404(ToimistoSuoritus, user=request.user)
-    users = ToimistoSuoritus.objects.all().order_by('time')
-    
-    return render(request, 'main2014/toimisto/kiitos.html', {
-        'event_id': 8,
-        'participant': par,
-        'users': users,
     })

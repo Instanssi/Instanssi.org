845c399fe7ac478f4a007d6a4183c3d1cd00ef2f
Tuomas Virtanen <katajakasa@gmail.com>
2018-07-11 21:28:36 +0300

    Fix ASCII file storage for django 2

--- Instanssi/common/storage.py
+++ Instanssi/common/storage.py
@@ -6,5 +6,5 @@ from django.core.files.storage import FileSystemStorage
 
 class ASCIIFileSystemStorage(FileSystemStorage):
     def get_valid_name(self, name):
-        name = unicodedata.normalize('NFKD', name).encode('ascii', 'ignore')
+        name = unicodedata.normalize('NFKD', name).encode('ascii', 'ignore').decode()
         return super(ASCIIFileSystemStorage, self).get_valid_name(name)

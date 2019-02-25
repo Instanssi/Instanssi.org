552b0b718ff134b9bd7519b1f4dbcaa3a98c8266
Tommi Teistela <totateis@gmail.com>
2019-02-25 23:08:11 +0200

    main2019: Remove old lighting effects

--- Instanssi/main2019/static/main2019/js/base.js
+++ Instanssi/main2019/static/main2019/js/base.js
@@ -42,48 +42,4 @@ $(function () {
         }, 500);
     }
     halos();
-
-    function backgrounds() {
-        var bgElements = [
-            '.layer-cyan',
-            '.layer-green',
-            '.layer-purple',
-            '.layer-yellow'
-        ].map(function(selector) {
-            return $(selector);
-        });
-
-        // minimum delay between animations
-        var DELAY = 2500;
-        // track whether the user is interacting with the page or not
-        var interacting = false;
-
-        function update() {
-            if(!interacting) {
-                return;
-            }
-            interacting = false;
-            // Using the clock for this keeps the look consistent when navigating.
-            var x = new Date().valueOf() / 2000;
-            bgElements.forEach(function(bgElement, index) {
-                bgElement.css('opacity', 0.5 + 0.5 * Math.sin((x + index) * Math.PI / 4));
-            });
-        }
-
-        function onInteraction() {
-            interacting = true;
-        }
-
-        document.addEventListener('mouseover', onInteraction);
-        document.addEventListener('mousedown', onInteraction);
-        document.addEventListener('keydown', onInteraction);
-
-        // Start the 2 s animation every 2+ s.
-        // The transitions sometimes glitch on Firefox if the opacity
-        // target value changes mid-transition.
-        // The gap isn't too noticeable, especially with ease-in-out.
-        setInterval(update, DELAY);
-        update();
-    }
-    backgrounds();
 });

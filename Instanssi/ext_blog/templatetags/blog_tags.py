fb730e453a3eb36bf2239dcf0059f98fa38e07d3
Tuomas Virtanen <katajakasa@gmail.com>
2022-03-23 23:37:12 +0200

    Dont show posts from earlier years in the blog

--- Instanssi/ext_blog/templatetags/blog_tags.py
+++ Instanssi/ext_blog/templatetags/blog_tags.py
@@ -9,7 +9,7 @@ register = template.Library()
 
 @register.inclusion_tag('ext_blog/blog_messages.html')
 def render_blog(event_id, max_posts=10):
-    entries = BlogEntry.objects.filter(event_id__lte=int(event_id), public=True).order_by('-date')[:max_posts]
+    entries = BlogEntry.objects.filter(event_id=int(event_id), public=True).order_by('-date')[:max_posts]
     return {'entries': entries}
 
 

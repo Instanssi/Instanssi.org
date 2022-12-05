c1ebdaf4697c6004139b2f3f796164b51683b116
Tuomas Virtanen <katajakasa@gmail.com>
2022-12-06 01:28:26 +0200

    Clean up infodesk

--- Instanssi/infodesk/urls.py
+++ Instanssi/infodesk/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path
 
 from Instanssi.infodesk.views import (
     index,
@@ -15,12 +15,12 @@ app_name = "infodesk"
 
 
 urlpatterns = [
-    url(r"^$", index, name="index"),
-    url(r"^item/check/", item_check, name="item_check"),
-    url(r"^transaction/check/", transaction_check, name="transaction_check"),
-    url(r"^item/info/(?P<item_id>\d+)/", item_info, name="item_info"),
-    url(r"^transaction/info/(?P<transaction_id>\d+)/", transaction_info, name="transaction_info"),
-    url(r"^item/mark/(?P<item_id>\d+)/", item_mark, name="item_mark"),
-    url(r"^order_search_ac", order_search_ac, name="order_search_ac"),
-    url(r"^order_search", order_search, name="order_search"),
+    path("", index, name="index"),
+    path("item/check/", item_check, name="item_check"),
+    path("transaction/check/", transaction_check, name="transaction_check"),
+    path("item/info/<int:item_id>/", item_info, name="item_info"),
+    path("transaction/info/<int:transaction_id>/", transaction_info, name="transaction_info"),
+    path("item/mark/<int:item_id>/", item_mark, name="item_mark"),
+    path("order_search_ac", order_search_ac, name="order_search_ac"),
+    path("order_search", order_search, name="order_search"),
 ]
--- Instanssi/infodesk/views.py
+++ Instanssi/infodesk/views.py
@@ -3,13 +3,18 @@ import logging
 import arrow
 from django.conf import settings
 from django.db.models import Q
-from django.http import Http404, HttpResponseRedirect
+from django.http import (
+    Http404,
+    HttpRequest,
+    HttpResponse,
+    HttpResponseRedirect,
+    JsonResponse,
+)
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.utils import timezone
 
 from Instanssi.common.auth import infodesk_access_required
-from Instanssi.common.responses import JSONResponse
 from Instanssi.infodesk.forms import ItemKeyScanForm, TransactionKeyScanForm
 from Instanssi.store.models import StoreTransaction, TransactionItem
 
@@ -20,23 +25,23 @@ TXN_ITEM_PREFIX = "item:"
 
 
 @infodesk_access_required
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     """Main infodesk view."""
     return render(request, "infodesk/index.html", {})
 
 
-def ta_fuzzy_query(term):
+def ta_fuzzy_query(term: str) -> Q:
     """Generate a fuzzy query filter for finding StoreTransactions."""
     return Q(lastname__icontains=term) | Q(firstname__icontains=term) | Q(key__istartswith=term)
 
 
-def ti_fuzzy_query(term):
+def ti_fuzzy_query(term: str) -> Q:
     """Generate a fuzzy query filter for finding TransactionItems."""
     return Q(key__istartswith=term)
 
 
 @infodesk_access_required
-def order_search_ac(request):
+def order_search_ac(request: HttpRequest) -> HttpResponse:
     """Autocomplete suggestions for the infodesk general purpose search.
 
     Each suggestion's "id" should find something in the order_search view.
@@ -46,7 +51,7 @@ def order_search_ac(request):
         raise Http404
 
     # find transactions that contain the search term
-    txns = StoreTransaction.objects.filter(ta_fuzzy_query(term))
+    transactions = StoreTransaction.objects.filter(ta_fuzzy_query(term))
     items = TransactionItem.objects.filter(ti_fuzzy_query(term))
 
     def fmt_t(t):
@@ -56,28 +61,28 @@ def order_search_ac(request):
         return "%s, %s (%s)" % (t.lastname, t.firstname, fmt_t(t.time_created))
 
     results = [
-        {"id": ("%s%s") % (TXN_PREFIX, t.id), "text": format_transaction(t)}  # exact query link
-        for t in txns
+        {"id": "%s%s" % (TXN_PREFIX, t.id), "text": format_transaction(t)}  # exact query link
+        for t in transactions
     ]
 
     results.extend(
         [
             {
-                "id": ("%s%s") % (TXN_ITEM_PREFIX, item.key),
+                "id": "%s%s" % (TXN_ITEM_PREFIX, item.key),
                 "text": "%s -- ostaja %s" % (item.item, format_transaction(item.transaction)),
             }
             for item in items
         ]
     )
 
-    return JSONResponse({"more": False, "results": results})
+    return JsonResponse({"more": False, "results": results})
 
 
 @infodesk_access_required
 def order_search(request):
     """Search view that finds transactions, items and customers."""
     term = request.GET.get("term")
-    txns = None
+    transactions = None
     items = None
 
     if term:
@@ -103,16 +108,16 @@ def order_search(request):
         if not item_filter:
             item_filter = ti_fuzzy_query(term)
 
-        txns = StoreTransaction.objects.filter(txn_filter)
+        transactions = StoreTransaction.objects.filter(txn_filter)
         items = TransactionItem.objects.filter(item_filter)
 
     return render(
-        request, "infodesk/order_search.html", {"transactions": txns, "items": items, "term": term}
+        request, "infodesk/order_search.html", {"transactions": transactions, "items": items, "term": term}
     )
 
 
 @infodesk_access_required
-def item_check(request):
+def item_check(request: HttpRequest) -> HttpResponse:
     if request.method == "POST":
         form = ItemKeyScanForm(request.POST)
         if form.is_valid():
@@ -124,7 +129,7 @@ def item_check(request):
 
 
 @infodesk_access_required
-def transaction_check(request):
+def transaction_check(request: HttpRequest) -> HttpResponse:
     if request.method == "POST":
         form = TransactionKeyScanForm(request.POST)
         if form.is_valid():
@@ -136,29 +141,28 @@ def transaction_check(request):
 
 
 @infodesk_access_required
-def item_mark(request, item_id):
+def item_mark(request: HttpRequest, item_id: int) -> HttpResponse:
     """Mark an item as delivered."""
     item = get_object_or_404(TransactionItem, pk=item_id)
     item.time_delivered = timezone.now()
     item.save()
     if item.transaction.is_paid:
-        logger.info("Item %d marked as delivered." % (item.id,), extra={"user": request.user})
+        logger.info("Item %d marked as delivered.", item.id, extra={"user": request.user})
     else:
         logger.info(
-            "Item %d marked as delivered (no payment recorded!)" % (item.id,),
-            extra={"user": request.user},
+            "Item %d marked as delivered (no payment recorded!)", item.id, extra={"user": request.user}
         )
 
     return HttpResponseRedirect(reverse("infodesk:item_info", args=(item.id,)))
 
 
 @infodesk_access_required
-def item_info(request, item_id):
+def item_info(request: HttpRequest, item_id: int) -> HttpResponse:
     item = get_object_or_404(TransactionItem, pk=item_id)
     return render(request, "infodesk/item_info.html", {"item": item})
 
 
 @infodesk_access_required
-def transaction_info(request, transaction_id):
+def transaction_info(request: HttpRequest, transaction_id: int) -> HttpResponse:
     transaction = get_object_or_404(StoreTransaction, pk=transaction_id)
     return render(request, "infodesk/ta_info.html", {"transaction": transaction})

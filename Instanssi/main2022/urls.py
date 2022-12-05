a0fa27fe391c17c0fb07cafd70800018e765ce41
Tuomas Virtanen <katajakasa@gmail.com>
2022-12-06 01:28:45 +0200

    Clean up main2022

--- Instanssi/main2022/urls.py
+++ Instanssi/main2022/urls.py
@@ -1,25 +1,20 @@
-from django.conf.urls import url
+from django.urls import path
 
-from Instanssi.main2022.views import pageloader
+from Instanssi.main2022.views import page_loader
 
 app_name = "main2022"
 
 urlpatterns = [
-    url(r"^$", pageloader, {"templatename": "index"}, name="index"),
-    url(r"^info/$", pageloader, {"templatename": "info"}, name="info"),
-    url(r"^english/$", pageloader, {"templatename": "english"}, name="english"),
-    url(r"^yhteystiedot/$", pageloader, {"templatename": "info"}, name="yhteystiedot"),
-    url(r"^ohjelma/$", pageloader, {"templatename": "ohjelma"}, name="ohjelma"),
-    url(r"^aikataulu/$", pageloader, {"templatename": "aikataulu"}, name="aikataulu"),
-    url(r"^kompot/$", pageloader, {"templatename": "kompot"}, name="kompot"),
-    url(
-        r"^kilpailusopimus/$",
-        pageloader,
-        {"templatename": "kilpailusopimus"},
-        name="kilpailusopimus",
-    ),
-    url(r"^stream/$", pageloader, {"templatename": "stream"}, name="stream"),
-    url(r"^saannot/$", pageloader, {"templatename": "saannot"}, name="saannot"),
-    url(r"^valot/$", pageloader, {"templatename": "valot"}, name="valot"),
-    url(r"^radio/$", pageloader, {"templatename": "radio"}, name="radio"),
+    path("", page_loader, {"template_name": "index"}, name="index"),
+    path("info/", page_loader, {"template_name": "info"}, name="info"),
+    path("english/", page_loader, {"template_name": "english"}, name="english"),
+    path("yhteystiedot/", page_loader, {"template_name": "info"}, name="yhteystiedot"),
+    path("ohjelma/", page_loader, {"template_name": "ohjelma"}, name="ohjelma"),
+    path("aikataulu/", page_loader, {"template_name": "aikataulu"}, name="aikataulu"),
+    path("kompot/", page_loader, {"template_name": "kompot"}, name="kompot"),
+    path("kilpailusopimus/", page_loader, {"template_name": "kilpailusopimus"}, name="kilpailusopimus"),
+    path("stream/", page_loader, {"template_name": "stream"}, name="stream"),
+    path("saannot/", page_loader, {"template_name": "saannot"}, name="saannot"),
+    path("valot/", page_loader, {"template_name": "valot"}, name="valot"),
+    path("radio/", page_loader, {"template_name": "radio"}, name="radio"),
 ]

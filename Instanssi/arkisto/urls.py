72638366e4ce22ab076bf554f36f2fc7188532a0
Tuomas Virtanen <katajakasa@gmail.com>
2022-12-06 01:27:55 +0200

    Clean up arkisto

--- Instanssi/arkisto/urls.py
+++ Instanssi/arkisto/urls.py
@@ -1,15 +1,22 @@
-from django.conf.urls import url
+from django.urls import path
 
-from Instanssi.arkisto.views import entry, event, index, json_event, text_event, video
+from Instanssi.arkisto.views import (
+    entry_index,
+    event_index,
+    index,
+    json_event,
+    text_event,
+    video_index,
+)
 
 app_name = "arkisto"
 
 
 urlpatterns = [
-    url(r"^$", index, name="index"),
-    url(r"^entry/(?P<entry_id>\d+)/", entry, name="entry"),
-    url(r"^text_event/(?P<event_id>\d+)/", text_event, name="text_event"),
-    url(r"^json_event/(?P<event_id>\d+)/", json_event, name="json_event"),
-    url(r"^event/(?P<event_id>\d+)/", event, name="event"),
-    url(r"^video/(?P<video_id>\d+)/", video, name="video"),
+    path("", index, name="index"),
+    path("entry/<int:entry_id>/", entry_index, name="entry"),
+    path("text_event/<int:event_id>/", text_event, name="text_event"),
+    path("json_event/<int:event_id>/", json_event, name="json_event"),
+    path("event/<int:event_id>/", event_index, name="event"),
+    path("video/<int:video_id>/", video_index, name="video"),
 ]
--- Instanssi/arkisto/views.py
+++ Instanssi/arkisto/views.py
@@ -1,11 +1,10 @@
 from copy import copy
 
-from django.http import Http404
+from django.http import Http404, HttpRequest, HttpResponse
 from django.http.response import JsonResponse
 from django.shortcuts import get_object_or_404, render
 
 from Instanssi.arkisto.models import OtherVideo, OtherVideoCategory
-from Instanssi.common.misc import get_url
 from Instanssi.kompomaatti.misc import entrysort
 from Instanssi.kompomaatti.models import (
     Competition,
@@ -16,37 +15,33 @@ from Instanssi.kompomaatti.models import (
 )
 
 
-def text_event(request, event_id):
-    _event = get_object_or_404(Event, pk=int(event_id), archived=True)
+def text_event(request: HttpRequest, event_id: int) -> HttpResponse:
+    event = get_object_or_404(Event, pk=event_id, archived=True)
 
     # Get all compos that are active but not hidden from archive
     compos = []
-    for compo in Compo.objects.filter(event=_event, active=True, hide_from_archive=False):
+    for compo in Compo.objects.filter(event=event, active=True, hide_from_archive=False):
         if compo.show_voting_results:
             compo.entries = entrysort.sort_by_score(Entry.objects.filter(compo=compo))
         else:
             compo.entries = Entry.objects.filter(compo=compo).order_by("name")
         compos.append(compo)
 
-    # Render Event frontpage
     return render(
         request,
         "arkisto/results.txt",
-        {
-            "event": _event,
-            "compos": compos,
-        },
+        {"event": event, "compos": compos},
         content_type="text/plain; charset=utf-8",
     )
 
 
-def json_event(request, event_id):
-    _event = get_object_or_404(Event, pk=int(event_id), archived=True)
+def json_event(request: HttpRequest, event_id: int) -> HttpResponse:
+    event = get_object_or_404(Event, pk=event_id, archived=True)
 
     # Get all compos that are active but not hidden from archive
     entries_out = []
     compos_out = []
-    for c in Compo.objects.filter(event=_event, active=True, hide_from_archive=False):
+    for c in Compo.objects.filter(event=event, active=True, hide_from_archive=False):
         if c.show_voting_results:
             entries = entrysort.sort_by_score(Entry.objects.filter(compo=c))
         else:
@@ -64,14 +59,18 @@ def json_event(request, event_id):
                     "entry_author": e.creator,
                     "entry_score": round(e.get_score(), 2),
                     "entry_rank": e.get_rank(),
-                    "entry_result_url": get_url(e.entryfile.url),
-                    "entry_source_url": get_url(e.sourcefile.url) if e.sourcefile else None,
+                    "entry_result_url": request.build_absolute_uri(e.entryfile.url),
+                    "entry_source_url": request.build_absolute_uri(e.sourcefile.url)
+                    if e.sourcefile
+                    else None,
                     "entry_youtube_url": e.youtube_url if e.youtube_url else None,
-                    "entry_image_thumbnail": get_url(e.imagefile_thumbnail.url)
+                    "entry_image_thumbnail": request.build_absolute_uri(e.imagefile_thumbnail.url)
                     if e.imagefile_thumbnail
                     else None,
-                    "entry_image_medium": get_url(e.imagefile_medium.url) if e.imagefile_medium else None,
-                    "entry_image_original": get_url(e.imagefile_original.url)
+                    "entry_image_medium": request.build_absolute_uri(e.imagefile_medium.url)
+                    if e.imagefile_medium
+                    else None,
+                    "entry_image_original": request.build_absolute_uri(e.imagefile_original.url)
                     if e.imagefile_original
                     else None,
                 }
@@ -80,16 +79,15 @@ def json_event(request, event_id):
     return JsonResponse({"entries": entries_out, "compos": compos_out})
 
 
-def event(request, event_id):
-    # Get event
-    event_obj = get_object_or_404(Event, pk=event_id, archived=True)
+def event_index(request, event_id):
+    event = get_object_or_404(Event, pk=event_id, archived=True)
 
     # Filter compos, videos and competitions
-    compos_q = Compo.objects.filter(event=event_obj, active=True, hide_from_archive=False).order_by("name")
-    videos_q = OtherVideoCategory.objects.filter(event=event_obj).order_by("name")
-    competitions_q = Competition.objects.filter(
-        event=event_obj, active=True, hide_from_archive=False
-    ).order_by("name")
+    compos_q = Compo.objects.filter(event=event, active=True, hide_from_archive=False).order_by("name")
+    videos_q = OtherVideoCategory.objects.filter(event=event).order_by("name")
+    competitions_q = Competition.objects.filter(event=event, active=True, hide_from_archive=False).order_by(
+        "name"
+    )
 
     # Get all compos that are active but not hidden from archive
     compo_list = []
@@ -109,74 +107,48 @@ def event(request, event_id):
     # Get competitions
     competition_list = []
     for comp in competitions_q:
-        rankby = "-score"
-        if comp.score_sort == 1:
-            rankby = "score"
-        comp.participants = CompetitionParticipation.objects.filter(competition=comp).order_by(rankby)
+        rank_by = "score" if comp.score_sort == 1 else "-score"
+        comp.participants = CompetitionParticipation.objects.filter(competition=comp).order_by(rank_by)
         competition_list.append(copy(comp))
 
     # Render Event frontpage
     return render(
         request,
         "arkisto/index.html",
-        {
-            "event": event_obj,
-            "compos": compo_list,
-            "videos": video_list,
-            "competitions": competition_list,
-        },
+        {"event": event, "compos": compo_list, "videos": video_list, "competitions": competition_list},
     )
 
 
 # Index page (loads event page)
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     try:
         latest = Event.objects.filter(archived=True).latest("date")
     except Event.DoesNotExist:
         return render(request, "arkisto/empty.html")
 
-    return event(request, latest.id)
+    return event_index(request, latest.id)
 
 
 # Entry page
-def entry(request, entry_id):
-    # Get the entry
-    _entry = get_object_or_404(Entry, pk=entry_id)
+def entry_index(request: HttpRequest, entry_id: int) -> HttpResponse:
+    entry = get_object_or_404(Entry, pk=entry_id)
 
     # Make sure the entry belongs to an archived event
-    if not _entry.compo.event.archived:
+    if not entry.compo.event.archived:
         raise Http404
 
     # Make sure the compo is active and can be shown
-    if _entry.compo.hide_from_archive or not _entry.compo.active:
+    if entry.compo.hide_from_archive or not entry.compo.active:
         raise Http404
 
     # Dump the page
-    return render(
-        request,
-        "arkisto/entry.html",
-        {
-            "entry": _entry,
-            "event": _entry.compo.event,
-        },
-    )
+    return render(request, "arkisto/entry.html", {"entry": entry, "event": entry.compo.event})
 
 
 # Video page
-def video(request, video_id):
-    # Get the entry
-    _video = get_object_or_404(OtherVideo, pk=video_id)
-
-    # Make sure the entry belongs to an archived event
-    if not _video.category.event.archived:
+def video_index(request: HttpRequest, video_id: int) -> HttpResponse:
+    video = get_object_or_404(OtherVideo, pk=video_id)
+    if not video.category.event.archived:
         raise Http404
 
-    # Dump the page
-    return render(
-        request,
-        "arkisto/video.html",
-        {
-            "video": _video,
-            "event": _video.category.event,
-        },
-    )
+    return render(request, "arkisto/video.html", {"video": video, "event": video.category.event})

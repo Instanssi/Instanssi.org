b4deeb9e750e9e714d040f70fd3b29555f7d0e63
Tuomas Virtanen <katajakasa@gmail.com>
2022-12-07 21:24:10 +0200

    Drop last old style unit-tests
    These were already moved to pytest side

--- Instanssi/common/testing/utils.py
+++
@@ -1,9 +0,0 @@
-from django.urls import reverse
-from django.utils.http import urlencode
-
-
-def q_reverse(view_name, query=None, **kwargs):
-    url = reverse(view_name, kwargs=kwargs)
-    if query:
-        return "{}?{}".format(url, urlencode(query))
-    return url

393f3186bba6e23a46e1e559dba63780d8408ae8
Tuomas Virtanen <katajakasa@gmail.com>
2022-12-10 03:13:10 +0200

    Add unit-tests for main2022 site

---
+++ tests/main2022/test_2022.py
@@ -0,0 +1,26 @@
+import pytest
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "site_name",
+    [
+        "index",
+        "info",
+        "english",
+        "ohjelma",
+        "aikataulu",
+        "kompot",
+        "kilpailusopimus",
+        "stream",
+        "saannot",
+        "valot",
+        "radio",
+    ],
+)
+def test_2023_site(page_client, site_name: str):
+    path = reverse(f"main2022:{site_name}")
+    response = page_client.get(path)
+    assert response.status_code == 200
+    assert "<body>" in response.content.decode()

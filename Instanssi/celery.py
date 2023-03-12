85adbae62b0b9b82648d03fc43c5b01f9a052de2
Tuomas Virtanen <katajakasa@gmail.com>
2023-03-12 16:47:14 +0200

    Drop unused celery task

--- Instanssi/celery.py
+++ Instanssi/celery.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import
 
 import os
-from typing import Any
 
 from celery import Celery
 
@@ -10,8 +9,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Instanssi.settings")
 app = Celery("proj")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
-
-
-@app.task(bind=True)
-def debug_task(self: Any) -> None:
-    print("Request: {0!r}".format(self.request))

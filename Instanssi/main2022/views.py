f822b7ad6915374cdff98080344f9d5bf8d4a3e3
Tuomas Virtanen <katajakasa@gmail.com>
2023-09-19 17:27:06 +0300

    Drop old main2022 and main2023 sites

--- Instanssi/main2022/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
-
-
-def page_loader(request: HttpRequest, template_name: str) -> HttpResponse:
-    return render(request, f"main2022/{template_name}.html", {"event_id": 21, "templatename": template_name})
--- Instanssi/main2023/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
-
-
-def page_loader(request: HttpRequest, template_name: str) -> HttpResponse:
-    # Instanssi ID is 22 in production database, so we hardcode that.
-    return render(request, f"main2023/{template_name}.html", {"event_id": 22, "templatename": template_name})

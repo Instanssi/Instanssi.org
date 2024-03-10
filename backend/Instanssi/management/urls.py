b8ea604167c240ac5dde11a56097ab872a21c705
Tuomas Virtanen <katajakasa@gmail.com>
2024-03-11 00:47:25 +0200

    backend: Make a project for prebuilt admin static files

--- backend/Instanssi/management/urls.py
+++ backend/Instanssi/management/urls.py
@@ -1,9 +1,13 @@
+from pathlib import Path
+
+from django.conf.urls.static import static
 from django.urls import path
 
-from Instanssi.management.views import dummy_index
+from Instanssi.management.views import management_index
 
 app_name = "management"
+CURRENT_DIR = Path(__file__).resolve(strict=True).parent
 
 urlpatterns = [
-    path("", dummy_index, name="index"),
-]
+    path("", management_index, name="index"),
+] + static("/assets/", document_root=CURRENT_DIR / "site" / "assets")
--- backend/Instanssi/management/views.py
+++ backend/Instanssi/management/views.py
@@ -1,5 +1,10 @@
-from django.http import Http404
+from pathlib import Path
+
+from django.http import HttpRequest, HttpResponse
+
+CURRENT_DIR = Path(__file__).resolve(strict=True).parent
 
 
-def dummy_index(request, *args, **kwargs):
-    raise Http404()
+def management_index(request: HttpRequest, *args, **kwargs):
+    with open(CURRENT_DIR / "site" / "index.html", "rb") as fd:
+        return HttpResponse(fd.read(), status=200, content_type="text/html")

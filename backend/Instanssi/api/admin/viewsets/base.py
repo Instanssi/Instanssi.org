3bbadea3f58513cbea8339160d395bd8a4cbd668
Tuomas Virtanen <katajakasa@gmail.com>
2024-03-13 18:24:31 +0200

    backend: Check user permissions in admin model APIs

--- backend/Instanssi/api/admin/viewsets/base.py
+++ backend/Instanssi/api/admin/viewsets/base.py
@@ -1,10 +1,10 @@
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.permissions import IsAdminUser, IsAuthenticated, DjangoModelPermissions
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
 
 class AdminReadOnlyViewSet(ReadOnlyModelViewSet):
-    permission_classes = [IsAuthenticated, IsAdminUser]
+    permission_classes = [IsAuthenticated, IsAdminUser, DjangoModelPermissions]
 
 
 class AdminViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated, IsAdminUser]
+    permission_classes = [IsAuthenticated, IsAdminUser, DjangoModelPermissions]

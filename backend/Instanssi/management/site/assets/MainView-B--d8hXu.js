b3ce524aa95da151115aa4c13bcae573ab460e0e
Tuomas Virtanen <katajakasa@gmail.com>
2024-03-28 16:40:25 +0200

    admin: Add compiled version of the new admin site

---
+++ backend/Instanssi/management/site/assets/MainView-B--d8hXu.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./LayoutBase.vue_vue_type_script_setup_true_lang-C5omT869.js";import{h as n,k as a,G as o,u as s,x as i}from"./index-BOkRKjps.js";import"./VRow-BJ5jiL8l.js";const u=n({__name:"MainView",setup(r){const{t:e}=a();return(_,c)=>(i(),o(t,{title:s(e)("MainView.title")},null,8,["title"]))}});export{u as default};
+//# sourceMappingURL=MainView-B--d8hXu.js.map

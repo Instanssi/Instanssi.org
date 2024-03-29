0a551d3ded4cd880fd68909adc3c255e486bc6b2
Tuomas Virtanen <katajakasa@gmail.com>
2024-03-30 01:54:09 +0200

    admin: Update built site

--- backend/Instanssi/management/site/assets/MainView-CKXhrckQ.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./LayoutBase.vue_vue_type_script_setup_true_lang-C2uo4hv-.js";import{j as n,l as a,D as o,u as s,y as i}from"./index-Dc_14tgw.js";import"./VRow-BqJ1k-Cp.js";const u=n({__name:"MainView",setup(r){const{t:e}=a();return(_,c)=>(i(),o(t,{title:s(e)("MainView.title")},null,8,["title"]))}});export{u as default};
-//# sourceMappingURL=MainView-CKXhrckQ.js.map

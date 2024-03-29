6c89b55fe676b7796cce2ca561939fb7a1b07f1d
Tuomas Virtanen <katajakasa@gmail.com>
2024-03-29 19:20:10 +0200

    admin: Slide in vue-toastification

--- admin/src/main.ts
+++ admin/src/main.ts
@@ -1,11 +1,13 @@
 import "@/assets/main.scss";
 import "@vueup/vue-quill/dist/vue-quill.snow.css";
+import "vue-toastification/dist/index.css";
 
 // Libs
 import { createApp } from "vue";
 import { createVuetify } from "vuetify";
 import { createI18n } from "vue-i18n";
 import { aliases, fa } from "vuetify/iconsets/fa-svg";
+import toast, { type PluginOptions } from "vue-toastification";
 
 // Our own stuff
 import App from "@/App.vue";
@@ -35,12 +37,18 @@ function init() {
             en: enLocale["datetimeFormats"],
         },
     });
+    const toastOptions: PluginOptions = {
+        maxToasts: 2,
+    };
 
     setupIcons(app);
     app.use(router);
     app.use(vuetify);
     app.use(i18n);
+    app.use(toast, toastOptions);
     app.mount("#app");
 }
 
+// Check it we are logged in first, and only then initialize the application.
+// This way we can already make decisions in the router about which page to show.
 useAuth().refreshStatus().then(init);

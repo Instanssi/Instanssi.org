59a3e32b785e7bfaa9f21665d34182cef38b52ef
Tuomas Virtanen <katajakasa@gmail.com>
2024-04-02 01:17:10 +0300

    backend: Update static admin site

--- backend/Instanssi/management/site/assets/MainView-CdYygNkz.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./LayoutBase.vue_vue_type_script_setup_true_lang-BVanmNRw.js";import{j as n,l as a,D as o,u as s,y as i}from"./index-D2OhOofM.js";import"./VRow-CwdARop4.js";const u=n({__name:"MainView",setup(r){const{t:e}=a();return(_,c)=>(i(),o(t,{title:s(e)("MainView.title")},null,8,["title"]))}});export{u as default};
-//# sourceMappingURL=MainView-CdYygNkz.js.map

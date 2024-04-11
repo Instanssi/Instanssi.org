5de1b74bb09e4f84b41f0785313161b584c8b56c
Tuomas Virtanen <katajakasa@gmail.com>
2024-04-12 01:47:20 +0300

    admin: Add missing API models

---
+++ admin/src/api/models/PaginatedUserList.ts
@@ -0,0 +1,15 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+
+/* istanbul ignore file */
+
+/* tslint:disable */
+
+/* eslint-disable */
+import type { User } from "./User";
+
+export type PaginatedUserList = {
+    count: number;
+    next?: string | null;
+    previous?: string | null;
+    results: Array<User>;
+};

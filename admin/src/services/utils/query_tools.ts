6e174028c1a2e2e8f0a230029db837d94e320f6c
Tuomas Virtanen <katajakasa@gmail.com>
2024-04-18 23:59:04 +0300

    admin: Improve typing in query_tools.ts

--- admin/src/services/utils/query_tools.ts
+++ admin/src/services/utils/query_tools.ts
@@ -1,17 +1,28 @@
+type SortArgs = {
+    key: string;
+    order: "asc" | "desc";
+};
+
+// These should match Vuetify data-table args
 export type LoadArgs = {
     page: number;
     itemsPerPage: number;
-    sortBy: [
-        {
-            key: string;
-            order: "asc" | "desc";
-        },
-    ];
+    sortBy: SortArgs[];
     groupBy: any;
     search: string;
 };
 
-export function getLoadArgs(args: LoadArgs) {
+// These are passed to the API client
+export type ApiArgs = {
+    offset: number;
+    limit: number;
+    sortBy: string | undefined;
+};
+
+/**
+ * Converts vuetify arguments to API compatible form
+ */
+export function getLoadArgs(args: LoadArgs): ApiArgs {
     return {
         offset: (args.page - 1) * args.itemsPerPage,
         limit: args.itemsPerPage,
@@ -19,11 +30,14 @@ export function getLoadArgs(args: LoadArgs) {
     };
 }
 
-export function getSortString(args: LoadArgs) {
+/**
+ * Convert sort arguments from vuetify to API compatible form.
+ */
+export function getSortString(args: LoadArgs): string | undefined {
     if (args.sortBy.length <= 0) {
         return undefined;
     }
-    const sortBy = args.sortBy[0];
+    const sortBy: SortArgs = args.sortBy[0];
     if (sortBy.order === "asc") {
         return `${sortBy.key}`;
     } else {

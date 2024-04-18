6d16b00ac5397c47355327f7f16c1b0afd4753e9
Tuomas Virtanen <katajakasa@gmail.com>
2024-04-18 23:59:48 +0300

    admin: Add test for query_tools

---
+++ admin/tests/query_tools.spec.ts
@@ -0,0 +1,25 @@
+import { describe, it, expect } from 'vitest'
+import { getLoadArgs } from "@/services/utils/query_tools";
+
+
+describe('getLoadArgs', () => {
+    it('works with full input', () => {
+        const out = getLoadArgs({
+            page: 5,
+            itemsPerPage: 10,
+            sortBy: [
+                {
+                    key: "test",
+                    order: "desc",
+                }
+            ],
+            groupBy: undefined,
+            search: "search",
+        });
+        expect(out).toStrictEqual({
+            limit: 10,
+            offset: 40,
+            sortBy: "-test"
+        });
+    });
+});

09d19622bba47fda17b77562613d25fd96d072f2
Tommi Teistelä <totateis@gmail.com>
2025-11-23 00:16:30 +0200

    Make bubbles only display if JS is enabled
    Looks less broken than static bubbles that never move(?)

--- backend/Instanssi/main2026/static/main2026/js/base.js
+++ backend/Instanssi/main2026/static/main2026/js/base.js
@@ -15,24 +15,28 @@ $(function () {
     layoutButton();
 
     function bubbles() {
-        var bubbles = document.querySelectorAll('.kupla');
-
-        function applyPosition(element) {
+        function applyState(element) {
             var state = element._state;
+            var baseScale = Math.min(0.5, state.t);
+            var scaleX = baseScale + Math.sin(state.phase + state.t * 2.0) * 0.05;
+            var scaleY = baseScale + Math.cos(state.phase + state.t * 2.0) * 0.05;
             element.style.right = `${state.x}px`;
             element.style.top = `${state.y}px`;
-            element.style.opacity = `${state.opacity}`;
+            element.style.opacity = `${state.opacity * Math.min(1.0, state.t)}`;
+            element.style.transform = `scale(${scaleX}, ${scaleY})`;
         }
 
-        function resetBubble(element) {
+        function resetState(element) {
             element._state = {
                 x: Math.random() * 200,
-                y: Math.random() * 20 + 300,
+                y: Math.random() * 50 + 250,
                 vx: (Math.random() - 0.5) * 30,
                 vy: -9 - Math.random() * 8,
                 opacity: 0.8 + Math.random() * 0.2,
+                t: 0,
+                phase: Math.random() * 10,
             };
-            applyPosition(element);
+            applyState(element);
         }
         function tickBubble(element, dt) {
             var state = element._state;
@@ -43,29 +47,45 @@ $(function () {
 
             state.vy += (-5) * dt;
             state.vy += -state.vy * 0.01 * dt;
+
+            state.t += dt;
             
             if (state.y < -200) {
-                resetBubble(element);
+                resetState(element);
             } else {
-                applyPosition(element);
+                applyState(element);
             }
         }
-        bubbles.forEach(resetBubble);
-
+        
         var lastTime = 0;
         function run(time) {
             var dt = (lastTime ? time - lastTime : 0) / 1000;
-            //dt *= 10;
             lastTime = time;
-            // const time = new Date();
             bubbles.forEach(function (element) {
                 tickBubble(element, dt);
             });
             requestAnimationFrame(run);
         }
 
+        var bubbleTypes = [
+            { className: "kupla kupla1", src: "/static/main2026/images/web-kupla1.png" },
+            { className: "kupla kupla2", src: "/static/main2026/images/web-kupla2.png" },
+        ];
+        var bg = document.querySelector('.header-background');
+        var bubbles = [];
+        function addBubbles(count) {
+            for (var i = 0; i < count; i++) {
+                var bubbleType = bubbleTypes[i % bubbleTypes.length];
+                var bubble = document.createElement('img');
+                Object.assign(bubble, bubbleType);
+                bg.appendChild(bubble);
+                resetState(bubble);
+                tickBubble(bubble, Math.random() * 20 * Math.random());
+                bubbles.push(bubble);
+            }
+        }
+        addBubbles(5);
         requestAnimationFrame(run);
-
     }
     bubbles();
 });

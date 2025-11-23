4891a1891db8fb2d0c14a44030bbf826f95838a5
Tommi Teistelä <totateis@gmail.com>
2025-11-23 20:42:31 +0200

    main2026: Drop old sponsors from site footer

--- backend/Instanssi/main2026/views.py
+++ backend/Instanssi/main2026/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 
 def page_loader(request: HttpRequest, template_name: str) -> HttpResponse:
-    # Instanssi ID is 24 in production database, so we hardcode that.
+    # Instanssi ID is 25 in production database, so we hardcode that.
     return render(request, f"main2026/{template_name}.html", {"event_id": 25, "templatename": template_name})

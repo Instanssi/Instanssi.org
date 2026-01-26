38aaf3eb1769ff1804251b03ab4d62907f3bd92f
Tuomas Virtanen <katajakasa@gmail.com>
2026-01-26 22:55:51 +0200

    Improve regen_alts command

--- backend/Instanssi/kompomaatti/management/commands/regen_alts.py
+++ backend/Instanssi/kompomaatti/management/commands/regen_alts.py
@@ -1,3 +1,5 @@
+import sys
+
 from django.core.management.base import BaseCommand
 
 from Instanssi.kompomaatti.models import Entry
@@ -6,6 +8,16 @@ from Instanssi.kompomaatti.models import Entry
 class Command(BaseCommand):
     help = "regenerate entry alternate audio files"
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "-i", "--event", required=False, help="Only regenerate entries in this event", type=int
+        )
+
     def handle(self, *args, **options):
-        for entry in Entry.objects.iterator():
+        qs = Entry.objects.get_queryset()
+        if event_id := options.get("event"):
+            sys.stderr.write(f"Regenerating entries in event {event_id}\n")
+            qs = qs.filter(compo__event_id=int(event_id))
+        for entry in qs.iterator():
+            sys.stderr.write(f"Generating alternate audio files for entry {entry}\n")
             entry.generate_alternates()

b6068f69abb11ca00ed8b9872d5c5d66181b3521
Tuomas Virtanen <katajakasa@gmail.com>
2026-02-17 21:57:52 +0200

    ext_programme: replace Finnish strings with translatable English

--- backend/Instanssi/ext_programme/models.py
+++ backend/Instanssi/ext_programme/models.py
@@ -1,26 +1,17 @@
 from datetime import datetime, time
 from pathlib import Path
 
-import arrow
 from auditlog.registry import auditlog
 from django.conf import settings
 from django.db import models
+from django.utils.formats import date_format
+from django.utils.translation import gettext_lazy as _
 from imagekit.models import ImageSpecField
 from imagekit.processors import ResizeToFill
 
 from Instanssi.common.file_handling import clean_filename, generate_upload_path
 from Instanssi.common.html.fields import SanitizedHtmlField
 from Instanssi.kompomaatti.models import Event
-
-short_days = [
-    "Ma",
-    "Ti",
-    "Ke",
-    "To",
-    "Pe",
-    "La",
-    "Su",
-]
 
 
 def generate_icon_path(entry: "ProgrammeEvent", filename: str) -> str:
@@ -36,68 +27,57 @@ def generate_icon_path(entry: "ProgrammeEvent", filename: str) -> str:
 
 class ProgrammeEvent(models.Model):
     EVENT_TYPES = (
-        (0, "Yksinkertainen"),
-        (1, "Yksityiskohtainen"),
+        (0, _("Simple")),
+        (1, _("Detailed")),
     )
 
-    event = models.ForeignKey(Event, verbose_name="Tapahtuma", on_delete=models.PROTECT)
-    start = models.DateTimeField("Alku", help_text="Tapahtuman alkamisaika.", db_index=True)
-    end = models.DateTimeField("Loppu", help_text="Tapahtuman loppumisaika.", null=True, blank=True)
-    description = SanitizedHtmlField("Kuvaus", blank=True)
-    title = models.CharField("Otsikko", help_text="Lyhyt otsikko.", max_length=128)
-    presenters = models.CharField("Henkilöt", help_text="Esityksen pitäjät tms.", max_length=256, blank=True)
-    presenters_titles = models.CharField(
-        "Nimikkeet",
-        help_text="Henkilön arvo-, ammatti- tai virkanimike.",
-        max_length=256,
-        blank=True,
-    )
-    place = models.CharField(
-        "Paikka", help_text="Tarkka paikka tapahtuma-areenalla", max_length=64, blank=True
-    )
+    event = models.ForeignKey(Event, verbose_name=_("Event"), on_delete=models.PROTECT)
+    start = models.DateTimeField(_("Start"), db_index=True)
+    end = models.DateTimeField(_("End"), null=True, blank=True)
+    description = SanitizedHtmlField(_("Description"), blank=True)
+    title = models.CharField(_("Title"), max_length=128)
+    presenters = models.CharField(_("Presenters"), max_length=256, blank=True)
+    presenters_titles = models.CharField(_("Titles"), max_length=256, blank=True)
+    place = models.CharField(_("Place"), max_length=64, blank=True)
 
     # This is such a hackish solution that it makes me want to throw up. Oh well.
     icon_original = models.ImageField(
-        "Kuva 1", max_length=255, upload_to=generate_icon_path, help_text="Kuva 1 tapahtumalle.", blank=True
+        _("Image 1"),
+        max_length=255,
+        upload_to=generate_icon_path,
+        blank=True,
     )
     icon_small = ImageSpecField([ResizeToFill(64, 64)], source="icon_original", format="PNG")
     icon2_original = models.ImageField(
-        "Kuva 2", max_length=255, upload_to=generate_icon_path, help_text="Kuva 2 tapahtumalle.", blank=True
+        _("Image 2"),
+        max_length=255,
+        upload_to=generate_icon_path,
+        blank=True,
     )
     icon2_small = ImageSpecField([ResizeToFill(64, 64)], source="icon2_original", format="PNG")
 
-    email = models.EmailField(
-        "Sähköposti",
-        help_text="Tapahtumaan liittyvä sähköposti-osoite (esim. esiintyjän).",
-        blank=True,
-    )
-    home_url = models.URLField("Kotiurli", help_text="Tapahtumaan liittyvä URL.", blank=True)
-    twitter_url = models.URLField("Twitter", help_text="Tapahtumaan liittyvä Twitter-url.", blank=True)
-    github_url = models.URLField("Github", help_text="Tapahtumaan liittyvä Github-url", blank=True)
-    facebook_url = models.URLField("Facebook", help_text="Tapahtumaan liittyvä facebook-url.", blank=True)
-    linkedin_url = models.URLField("LinkedIn", help_text="Tapahtumaan liittyvä LinkedIn-url.", blank=True)
-    wiki_url = models.URLField("Wikipedia", help_text="Tapahtumaan liittyvä Wikipedia-url.", blank=True)
+    email = models.EmailField(_("Email"), blank=True)
+    home_url = models.URLField(_("Home URL"), blank=True)
+    twitter_url = models.URLField(_("Twitter"), blank=True)
+    github_url = models.URLField(_("Github"), blank=True)
+    facebook_url = models.URLField(_("Facebook"), blank=True)
+    linkedin_url = models.URLField(_("LinkedIn"), blank=True)
+    wiki_url = models.URLField(_("Wikipedia"), blank=True)
     event_type = models.IntegerField(
-        "Tapahtuman tyyppi",
+        _("Event type"),
         choices=EVENT_TYPES,
         default=0,
-        help_text="Määrittää tapahtuman tyypin. Yksityiskohtaiset tapahtumat näkyvät etusivun tapahtumalistassa.",
     )
-    active = models.BooleanField("Aktiivinen", help_text="Deaktivoidut piilotetaan.", default=True)
+    active = models.BooleanField(_("Active"), default=True)
 
     @property
     def short_start_time(self) -> str:
         if self.start:
-            start = arrow.get(self.start).to(settings.TIME_ZONE)
-            return "{} {}".format(short_days[start.weekday()], start.format("HH:mm"))
+            return date_format(self.start, "D H:i")
         return ""
 
     def __str__(self) -> str:
         return self.title
-
-    class Meta:
-        verbose_name = "ohjelmatapahtuma"
-        verbose_name_plural = "ohjelmatapahtumat"
 
 
 auditlog.register(ProgrammeEvent)

d11926c893c076593aef46cb5c7891e8513c54e2
Tuomas Virtanen <katajakasa@gmail.com>
2026-03-08 11:22:56 +0200

    store: Use defineConfig from vitest in vite config file

--- store/vite.config.ts
+++ store/vite.config.ts
@@ -1,5 +1,4 @@
-/// <reference types="vitest/config" />
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import { resolve } from "node:path";
 import { minifyHtmlTemplatePlugin } from "./minifyHtmlTemplatePlugin.js";
 
